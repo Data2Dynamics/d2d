fbe2001fce436df1dac5cfde120976857097b44c
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2013-03-22 13:34:29 +0100

    bug fixes and performance improve

--- arFramework3/arNLS.m
+++ arFramework3/arNLS.m
@@ -45,7 +45,7 @@ end
 
 % initial trust region size
 if(isempty(options.InitTrustRegionRadius))
-    mu = 0.01;         
+    mu = 1;         
 else
     mu = options.InitTrustRegionRadius;         
 end
@@ -56,7 +56,7 @@ mu_fac = 2;
 % counters
 iter = 0;
 funevals = 0;
-trust_calls = 0;
+solver_calls = 0;
 
 % initial function evaluation
 [res, sres] = feval(fun, p);
@@ -73,21 +73,33 @@ if(debug>2)
 end
 optimValues = struct([]);
 optimValues(1).iteration = 0;
+optimValues(1).mu = mu;
+optimValues(1).normdp = nan;
 
-dp = 1;
+% step inertia
+dpmem = [];
+
+dp = nan;
 dresnorm = -1;
-while(iter < options.MaxIter && dresnorm < 0 && norm(dp) >= options.TolX)
+while(iter < options.MaxIter && dresnorm < 0 && mu >= options.TolX)
     iter = iter + 1;
     
     % call output function
     if(~isempty(options.OutputFcn))
         feval(options.OutputFcn,[],optimValues,'iter');
         optimValues(1).iteration = optimValues(1).iteration + 1;
+        optimValues(1).mu = mu;
+        optimValues(1).normdp = norm(dp);
     end
     
     % solve subproblem
-    [dp, trust_calls, gred] = getStep(g, H, mu, p, lb, ub, trust_calls);
+    [dp, solver_calls, gred, dpmem] = ...
+        getStep(g, H, mu, p, lb, ub, solver_calls, dpmem);
     pt = p + dp;
+    
+    % ensure strict feasibility
+    pt(pt<lb) = lb(pt<lb);
+    pt(pt>ub) = ub(pt>ub);
     
     % function evaluation
     [rest, srest] = feval(fun, pt);
@@ -99,8 +111,13 @@ while(iter < options.MaxIter && dresnorm < 0 && norm(dp) >= options.TolX)
     
     % output
     if(debug>2)
-        fprintf('%3i/%3i  resnorm=%-8.2g  mu=%-8.2g  norm(dp)=%-8.2g  dresnorm=%-8.2g  norm(g)=%-8.2g  sub-dim=%i', ...
-            iter, options.MaxIter, resnorm, mu, norm(dp), dresnorm, norm(gred), length(gred));
+        if(dresnorm<0)
+            fprintf('%3i/%3i  resnorm=%-8.2g  mu=%-8.2g  norm(dp)=%-8.2g  dresnorm=%-8.2g  norm(g)=%-8.2g  sub-dim=%i', ...
+                iter, options.MaxIter, resnorm, mu, norm(dp), dresnorm, norm(gred), length(gred));
+        else
+            fprintf('%3i/%3i  resnorm=%-8.2g  mu=%-8.2g  norm(dp)=%-8.2g  dresnorm=%8s  norm(g)=%-8.2g  sub-dim=%i', ...
+                iter, options.MaxIter, resnorm, mu, norm(dp), '', norm(gred), length(gred));
+        end
     end
     
     if(norm(dp) >= options.TolX)
@@ -115,8 +132,13 @@ while(iter < options.MaxIter && dresnorm < 0 && norm(dp) >= options.TolX)
                 mu = mu / mu_fac; % shrinc trust region
                 
                 % solve subproblem
-                [dp, trust_calls, gred] = getStep(g, H, mu, p, lb, ub, trust_calls);
+                [dp, solver_calls, gred, dpmem] = ...
+                    getStep(g, H, mu, p, lb, ub, solver_calls, dpmem);
                 pt = p + dp;
+                
+                % ensure strict feasibility
+                pt(pt<lb) = lb(pt<lb);
+                pt(pt>ub) = ub(pt>ub);
                 
                 % function evaluation
                 [rest, srest] = feval(fun, pt);
@@ -128,16 +150,21 @@ while(iter < options.MaxIter && dresnorm < 0 && norm(dp) >= options.TolX)
                 
                 % output
                 if(debug>2)
-                    fprintf('%3i/%3i  resnorm=%-8.2g  mu=%-8.2g  norm(dp)=%-8.2g  dresnorm=%-8.2g  norm(g)=%-8.2g  sub-dim=%i', ...
-                        iter, options.MaxIter, resnorm, mu, norm(dp), dresnorm, norm(gred), length(gred));
+                    if(dresnorm<0)
+                        fprintf('%3i/%3i  resnorm=%-8.2g  mu=%-8.2g  norm(dp)=%-8.2g  dresnorm=%-8.2g  norm(g)=%-8.2g  sub-dim=%i', ...
+                            iter, options.MaxIter, resnorm, mu, norm(dp), dresnorm, norm(gred), length(gred));
+                    else
+                        fprintf('%3i/%3i  resnorm=%-8.2g  mu=%-8.2g  norm(dp)=%-8.2g  dresnorm=%8s  norm(g)=%-8.2g  sub-dim=%i', ...
+                            iter, options.MaxIter, resnorm, mu, norm(dp), '', norm(gred), length(gred));
+                    end
                 end
                 
-                if(norm(dp) < options.TolX)
+                if(mu < options.TolX)
                     break;
                 end
             end
         else
-            if(norm(dp)>0.9*mu)
+            if(norm(dp)>0.5*mu)
                 mu = mu * mu_fac;
             end
         end
@@ -169,7 +196,7 @@ end
 if(iter==options.MaxIter)
     exitflag = 0;
 end
-if(norm(dp)<options.TolX)
+if(mu<options.TolX)
     exitflag = 2;
 end
 
@@ -178,6 +205,7 @@ if (nargout>4)
     output = struct([]);
     output(1).iterations = iter;
     output.funcCount = funevals;
+    output.solverCount = solver_calls;
     output.algorithm = 'nls_trust';
     output.firstorderopt = norm(gred);
     switch(exitflag)
@@ -205,8 +233,6 @@ if(debug>1 || (debug>0 && exitflag==0))
 end
 
 
-
-function [dp, trust_calls, gred] = getStep(g, H, mu, p, lb, ub, trust_calls)
 % generate trial steps
 %
 % g:    gradient
@@ -215,40 +241,41 @@ function [dp, trust_calls, gred] = getStep(g, H, mu, p, lb, ub, trust_calls)
 % p:    current parameters
 % lb:   lower bounds
 % ub:   upper bounds
+function [dp, solver_calls, gred, dpmem, imem, dpmemsize] = getStep(g, H, mu, p, lb, ub, solver_calls, dpmem, imem, dpmemsize)
 
-dp = trust(-g',H,mu)'; % trust region solution
-trust_calls = trust_calls + 1;
-
-% PROBLEM: ensuring norm(dp)<=mu in trust function
-if(norm(dp)>mu)
-    dp = dp/norm(dp)*mu;
-end
+% solve subproblem
+dp = getDP(g, H, mu);
+solver_calls = solver_calls + 1;
 
 distp = -[p-ub; -(p - lb)]; % distance to bounds (should be always positive)
+onbound = distp==0; % which parameter is exactly on the bound ?
 exbounds = [p+dp-ub; -(p+dp - lb)] > 0; % dp bejond bound ?
 
 % if p on bounds and dp pointing bejond bound, reduce problem
-onbound = distp==0;
+gred = g;
 qred = sum(onbound & exbounds,1)>0;
 if(sum(~qred)==0)
     error('solution outside bounds, no further step possible');
 end
-if(sum(qred)>0)
-    gred = g(~qred);
-    Hred = H(~qred,~qred);
-    dp_red = trust(-gred',Hred,mu)'; % reduced trust region solution
-    trust_calls = trust_calls + 1;
+qred_presist = qred;
+while(sum(qred)>0)
+    gred = g(~qred_presist);
+    Hred = H(~qred_presist,~qred_presist);
     
-    % PROBLEM: ensuring norm(dp)<=mu in trust function
-    if(norm(dp_red)>mu)
-        dp_red = dp_red/norm(dp_red)*mu;
-    end
+    % solve reduced subproblem
+    dp_red = getDP(gred, Hred, mu);
+    solver_calls = solver_calls + 1;
     
     dp(:) = 0;
-    dp(~qred) = dp_red;
+    dp(~qred_presist) = dp_red;
+    
     exbounds = [p+dp-ub; -(p+dp - lb)] > 0; % dp bejond bound ?
-else
-    gred = g;
+    qred = sum(onbound & exbounds,1)>0;
+    qred_presist = qred | qred_presist;
+    
+    if(sum(~qred_presist)==0)
+        error('solution outside bounds, no further step possible');
+    end
 end
 
 % if dp too long cut to bounds
@@ -260,3 +287,24 @@ if(~isempty(dpredfac))
     end
     dp = dp * dpredfac;
 end
+
+% memory
+if(~isempty(dpmem))
+    dp = mean([dpmem;dp]);
+end
+dpmem = dp;
+
+
+
+% solver function
+function dp = getDP(g, H, mu)
+
+% trust region solution
+dp = trust(-g',H,mu)'; 
+% PROBLEM: ensuring norm(dp)<=mu in trust function
+if(norm(dp)>mu)
+    dp = dp/norm(dp)*mu;
+end
+
+% % levenberg-marquardt
+% dp = transpose(pinv(H + eye(size(H))/mu)*g');
