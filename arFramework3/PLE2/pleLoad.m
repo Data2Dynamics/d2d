b53f2618b1eb8a1aa5a09740fa4366366d3815ed
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2013-06-05 09:50:51 +0200

    changes in the arLoad Function. The new function does not require and "global ar" command to update the main variable after loading. As a drawback, there has to be a new function ar = arLoadFilename(filename); that implement the loading from a specified file that can be used in scripts.

--- arFramework3/PLE2/pleLoad.m
+++ arFramework3/PLE2/pleLoad.m
@@ -1,16 +1,16 @@
 % load PLE workspace if existing
 
-function pleLoad
-
-global ar
+function ple = pleLoad(ar)
 
 savepath = [ar.config.savepath '/PLE'];
 
 if(exist(savepath,'dir')==7)
-    load([savepath '/results.mat'])
+    S = load([savepath '/results.mat']);
+    ple = S.pleGlobals;
     fprintf(1,'PLE workspace loaded from file %s\n', [savepath '/results.mat']);
 else
     fprintf(1,'No saved PLE workspace found!\n');
+    ple = [];
 end
     
 
--- arFramework3/arLoadFilename.m
+++ arFramework3/arLoadFilename.m
@@ -1,15 +1,15 @@
 % load model struct and last ple results
 
+function [ar, pleGlobals] = arLoadFilename(filename)
+
 arCheck;
 
-global ar
-global pleGlobals
-
-[~, filename] = fileChooser('./Results', 1, true);
+if(~exist('filename', 'var'))
+    [~, filename] = fileChooser('./Results', 1, true);
+end
 
 S = load(['./Results/' filename '/workspace.mat']);
 ar = S.ar;
-
 fprintf('workspace loaded from file %s\n', filename);
 
 pleGlobals = pleLoad(ar);
