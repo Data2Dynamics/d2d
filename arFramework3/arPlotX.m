ec0b013d14d5032ff2f4a08b26e1bb5228fc1ad7
Bernhard Steiert <bernhard.steiert@fdm.uni-freiburg.de>
2013-07-18 11:05:37 +0200

    Ubuntu 13.04 compatibility fix for arPlotX

--- arFramework3/arPlotX.m
+++ arFramework3/arPlotX.m
@@ -559,7 +559,7 @@ savePath = mypath([savePath '/' name]);
 
 saveas(h, savePath, 'fig');
 print('-depsc2', savePath);
-eval(['!ps2pdf  -dEPSCrop ' savePath '.eps '  savePath '.pdf']);
+system(['export LD_LIBRARY_PATH=""; ps2pdf  -dEPSCrop ' savePath '.eps '  savePath '.pdf']);
 
 
 
