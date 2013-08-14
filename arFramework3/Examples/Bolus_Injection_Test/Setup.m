01698d9603276a43c76170af02151ff067dada39
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2013-08-14 10:19:25 +0200

    bolus injection example

---
+++ arFramework3/Examples/Bolus_Injection_Test/Setup.m
@@ -0,0 +1,20 @@
+% Load models & data
+
+arInit;
+arLoadModel('bolus_test',1);
+arParseModel;
+arWriteCFiles;
+arLink;
+
+%% set parameters for bolus injection
+
+% injection amount, here 5 nM
+arSetPars('bolus_amount', 5, 2, 0, 0, 10);
+
+% injection time point, here at 40 min
+arSetPars('injection_timepoint', 40, 2, 0, 0, 10);
+
+% injection duration, here at 1 min
+arSetPars('injection_duration', 1, 2, 0, 0, 10);
+
+arPlot;
