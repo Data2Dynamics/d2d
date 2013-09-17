6bd9b11206372739257bbce4fe66e05e04cc8721
Bernhard Steiert <bernhard.steiert@fdm.uni-freiburg.de>
2013-09-17 08:56:12 +0200

    Should fix Max' issue #6

---
+++ arFramework3/udata.h
@@ -0,0 +1,14 @@
+#ifndef _MY_UDATA
+#define _MY_UDATA
+typedef struct {
+	double *qpositivex;
+	double *u;
+	double *su;
+	double *p;
+	double *v;
+	double *sv;
+	double *dvdx;
+	double *dvdu;
+	double *dvdp;
+	} *UserData;
+#endif /* _MY_UDATA */
