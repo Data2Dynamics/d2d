16a6b2ee4517cfe8a17b7b2c8569baea14b6e5aa
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2013-11-22 09:31:25 -0500

    arCreateProject function, creates an empty project folder with template model and data .def files

---
+++ arFramework3/arCreateProject.m
@@ -0,0 +1,37 @@
+% create D2D file structure and template .def files
+
+function arCreateProject
+
+name = input('enter new project name: ', 's');
+
+if(~isempty(name))
+    path = ['./' name];
+    
+    if(exist(path, 'dir'))
+        fprintf('folder %s already exists!\n', path);
+    else
+        mkdir(path)
+        cd(path)
+        
+        mkdir('Models');
+        cd('Models');
+        copyfile(which('model_template.def'),'./model_template.def');
+        cd ..
+        
+        mkdir('Data');
+        cd('Data');
+        copyfile(which('data_template.def'),'./data_template.def');
+        cd ..
+        
+        fid = fopen('./Setup.m' , 'W');
+        
+        fprintf(fid, 'arInit;\n');
+        fprintf(fid, 'arLoadModel(''model_template'',1);\n');
+        fprintf(fid, 'arLoadData(''data_template'',1);\n');
+        fprintf(fid, 'arParseModel;\n');
+        fprintf(fid, 'arWriteCFiles;\n');
+        fprintf(fid, 'arLink;\n');
+        
+        fclose(fid);
+    end    
+end
