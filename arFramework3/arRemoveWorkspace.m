75519adba434538b40f26067db181e4e483eb966
Max Schelker <max.schelker@biologie.hu-berlin.de>
2014-08-08 12:47:02 +0200

    added function to remove saved Workspaces through fileChooser

---
+++ arFramework3/arRemoveWorkspace.m
@@ -0,0 +1,17 @@
+% Remove workspace folder of your choice
+%
+% Data-2-Dynamics Software
+% Website: https://bitbucket.org/d2d-development/d2d-software/wiki/Home
+% Contact: Andreas Raue - andreas.raue@fdm.uni-freiburg.de
+% Copyright 2013 D2D Development Team. All rights reserved.
+
+function arRemoveWorkspace
+arCheck;
+
+[~, filename] = fileChooser('./Results', 1, true);
+
+remove = input(sprintf('do you really want to remove the workspace \"%s\"? Y/[N]\n', filename),'s');
+if strcmpi(remove,'y')
+    system(['rm -R ./Results/' filename]);
+    fprintf('workspace \"%s\" successfully removed!\n', filename);
+end
