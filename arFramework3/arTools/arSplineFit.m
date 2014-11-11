f1d6fcc3d6b829b18c7662cb087d859a00d6f117
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2014-11-11 14:39:57 -0500

    added curvature constraints

--- arFramework3/arTools/arSplineFit.m
+++ arFramework3/arTools/arSplineFit.m
@@ -1,4 +1,4 @@
-function y = arSplineFit(xd, yd, xp, x, initialSlope)
+function y = arSplineFit(xd, yd, xp, x, initialSlope, curveConstr)
 
 if(~exist('x','var'))
     x = [];
@@ -6,12 +6,15 @@ end
 if(~exist('initialSlope','var'))
     initialSlope = [];
 end
+if(~exist('curveConstr','var'))
+    curveConstr = [];
+end
 
 yp0 = zeros(size(xp));
 lb = zeros(size(xp))-5;
 ub = zeros(size(xp))+5;
 opts = optimoptions('lsqnonlin','Display','off');
-yp = lsqnonlin(@(yp) mymerrit(xp, yp, xd, yd, initialSlope), yp0, lb, ub, opts);
+yp = lsqnonlin(@(yp) mymerrit(xp, yp, xd, yd, initialSlope, curveConstr), yp0, lb, ub, opts);
 
 if(isempty(x))
     y = spline(xp,yp);
@@ -21,7 +24,7 @@ end
 
 
 
-function F = mymerrit(xp, yp, xd, yd, initialSlope)
+function F = mymerrit(xp, yp, xd, yd, initialSlope, curveConstr)
 
 pp = spline(xp,yp);
 yp = ppval(pp,xd); 
@@ -30,4 +33,12 @@ F = yp(:)-yd(:);
 if(~isempty(initialSlope))
     ppdt = arSplineDer(pp);
     F(end+1) = ppval(ppdt,xp(1)) - initialSlope;
+end
+if(~isempty(initialSlope))
+    if(~exist('ppdt','var'))
+        ppdt = arSplineDer(pp);
+    end
+    ppdtdt = arSplineDer(ppdt);
+    tmpF = ppval(ppdtdt,xd) * curveConstr;
+    F = [F(:); tmpF(:)];
 end
