e7935c007571f8dbd874a7bf0caf21a72fe770c3
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2014-12-18 13:34:47 -0500

    bug fix

--- arFramework3/arPlotPriors.m
+++ arFramework3/arPlotPriors.m
@@ -1,6 +1,5 @@
 for i=1:50:length(ar.p)
     figure;
-    set(gcf,'Position',[500 5000 1500 1000])
     if i+50 > length(ar.p)
         xdata = linspace(i,length(ar.p),length(ar.p)-i+1);
         errorbar(xdata,ar.mean(i:length(ar.p)),ar.std(i:length(ar.p)),'b.');
@@ -11,9 +10,6 @@ for i=1:50:length(ar.p)
         plot(xdata,ar.p(i:length(ar.p)),'r*');
         set(gca,'XTick',xdata);
         set(gca,'XTickLabel',ar.pLabel(i:length(ar.p)));
-        set(gca,'FontSize',14);
-        set(gca,'FontWeight','bold');
-        set(gca,'Box','off');
         view(gca,[90 90]);
         hold off
     else
@@ -26,9 +22,6 @@ for i=1:50:length(ar.p)
         plot(xdata,ar.p(i:i+49),'r*');
         set(gca,'XTick',xdata);
         set(gca,'XTickLabel',ar.pLabel(i:i+49));
-        set(gca,'FontSize',14);
-        set(gca,'FontWeight','bold');
-        set(gca,'Box','off');
         view(gca,[90 90]);
         hold off
     end
