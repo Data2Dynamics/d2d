492ccc097f1a8ff390deca054af49eadbf79ebc8
Bernhard Steiert <bernhard.steiert@fdm.uni-freiburg.de>
2015-01-15 09:59:08 +0100

    Fix issue #25

--- arFramework3/arPlotY.m
+++ arFramework3/arPlotY.m
@@ -807,35 +807,12 @@ else
     system(['export LD_LIBRARY_PATH=""; ps2pdf  -dEPSCrop ' savePath '.eps '  savePath '.pdf']);
 end
 if(exist('plot2svg','file')==2)
+    myAxisPermutation(h)
     plot2svg([savePath '.svg'], h);
 end
 if(exist('matlab2tikz','file')==2)
     set(h,'Units','in')
-    
-    % Permute lines and patches
-    haxes = findobj(h, 'type', 'axes');
-    for iaxis = 1:length(haxes)
-        hlines = findobj(haxes(iaxis), 'type', 'line');
-        axischild = get(haxes(iaxis),'Children');
-        lineind = ismember(axischild,hlines);
-        hpatches = findobj(haxes(iaxis), 'type', 'patch');
-        patchind = find(ismember(axischild,hpatches));
-        patchind_sorted = [];
-        for ipatch = patchind'
-            if(isnumeric(get(axischild(ipatch),'FaceColor')))
-                % Patch with FaceColor to the background
-                patchind_sorted = [patchind_sorted ipatch];
-            else
-                % Patch without FaceColor to the front
-                patchind_sorted = [ipatch patchind_sorted];
-            end
-        end
-        % Find other handles (e.g. text)
-        otherhandles = ~ismember(1:length(axischild),[find(lineind)' patchind_sorted]);
-        % Lines to the front, then patches, then other handles
-        set(haxes(iaxis),'Children',[axischild(lineind); axischild(patchind_sorted); axischild(otherhandles)])
-    end
-    
+    myAxisPermutation(h)
     matlab2tikz([savePath '.tex'],'figurehandle',h,'showInfo', false, 'width','0.9\textwidth')
 end
 
@@ -866,3 +843,28 @@ if(nrows*ncols == ny)
     [nrows, ncols] = arNtoColsAndRows(ny+1, rowstocols);
 end
 
+
+function myAxisPermutation(h)
+% Permute lines and patches
+haxes = findobj(h, 'type', 'axes');
+for iaxis = 1:length(haxes)
+    hlines = findobj(haxes(iaxis), 'type', 'line');
+    axischild = get(haxes(iaxis),'Children');
+    lineind = ismember(axischild,hlines);
+    hpatches = findobj(haxes(iaxis), 'type', 'patch');
+    patchind = find(ismember(axischild,hpatches));
+    patchind_sorted = [];
+    for ipatch = patchind'
+        if(isnumeric(get(axischild(ipatch),'FaceColor')))
+            % Patch with FaceColor to the background
+            patchind_sorted = [patchind_sorted ipatch];
+        else
+            % Patch without FaceColor to the front
+            patchind_sorted = [ipatch patchind_sorted];
+        end
+    end
+    % Find other handles (e.g. text)
+    otherhandles = ~ismember(1:length(axischild),[find(lineind)' patchind_sorted]);
+    % Lines to the front, then patches, then other handles
+    set(haxes(iaxis),'Children',[axischild(lineind); axischild(patchind_sorted); axischild(otherhandles)])
+end
