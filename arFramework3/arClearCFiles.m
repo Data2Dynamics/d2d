e5a6830effa824fa06742ab2bf1ccff0dac7f9f1
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2015-02-10 19:07:19 -0500

    option to not remove the final mex file but clear the intermediate c files

--- arFramework3/arClearCFiles.m
+++ arFramework3/arClearCFiles.m
@@ -1,12 +1,18 @@
 % clear compiled c-files
 
-function arClearCFiles
+function arClearCFiles(doRemoveMex)
 
-filelist = dir;
-for j=1:length(filelist)
-    indexes = strfind(filelist(j).name, 'arSimuCalcFun_');
-    if(~isempty(indexes))
-        delete(filelist(j).name);
+if(nargin==0)
+    doRemoveMex = true;
+end
+
+if(doRemoveMex)
+    filelist = dir;
+    for j=1:length(filelist)
+        indexes = strfind(filelist(j).name, 'arSimuCalcFun_');
+        if(~isempty(indexes))
+            delete(filelist(j).name);
+        end
     end
 end
 
