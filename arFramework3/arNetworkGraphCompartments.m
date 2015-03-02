68710786d057a031cb673d1865cd2b1bc3aee81d
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2015-03-02 09:46:58 -0500

    improvements in arNetworkGraphCompartments function

--- arFramework3/arNetworkGraphCompartments.m
+++ arFramework3/arNetworkGraphCompartments.m
@@ -38,35 +38,36 @@ fprintf(fid, 'digraph G {\n');
 fprintf(fid, '\tgraph [overlap=false];\n');
 
 % measured?
-qu = false(size(ar.model(m).us));
-qx = false(size(ar.model(m).xs));
-if(isfield(ar.model(m), 'data') && isfield(ar.model(1).data(1), 'qu_measured'))
-    for jd=1:length(ar.model(m).data)
-        if(~isempty(ar.model(m).us) && ~isempty(ar.model(m).data(jd).qu_measured))
-            qu = qu | ar.model(m).data(jd).qu_measured;
-        end
-        if(~isempty(ar.model(m).data(jd).qx_measured))
-            qx = qx | ar.model(m).data(jd).qx_measured;
+if(isfield(ar.model(m), 'data'))
+    qu = false(size(ar.model(m).us));
+    qx = false(size(ar.model(m).xs));
+    if(isfield(ar.model(1).data(1), 'qu_measured'))
+        for jd=1:length(ar.model(m).data)
+            if(~isempty(ar.model(m).us) && ~isempty(ar.model(m).data(jd).qu_measured))
+                qu = qu | ar.model(m).data(jd).qu_measured;
+            end
+            if(~isempty(ar.model(m).data(jd).qx_measured))
+                qx = qx | ar.model(m).data(jd).qx_measured;
+            end
         end
     end
+else
+    qu = true(size(ar.model(m).us));
+    qx = true(size(ar.model(m).xs));
 end
 
 % measured inputs
 if(sum(qu)>0)
-    fprintf(fid, '\t{node [shape=diamond];');
     for j=find(qu)
-        fprintf(fid, '%s ', ar.model(m).u{j});
+        fprintf(fid, '\t{node [shape=diamond]; %s;}\n', ar.model(m).u{j});
     end
-    fprintf(fid, ';}\n');
 end
 
 % not measured inputs
 if(sum(~qu)>0)
-    fprintf(fid, '\t{node [style=dashed];');
     for j=find(~qu)
-        fprintf(fid, '%s ', ar.model(m).u{j});
+        fprintf(fid, '\t{node [style=dashed]; %s;}\n', ar.model(m).u{j});
     end
-    fprintf(fid, ';}\n');
 end
 
 for jc = 1:length(ar.model(m).c)
@@ -76,24 +77,19 @@ for jc = 1:length(ar.model(m).c)
     
     % measured species
     if(sum(qx & qc)>0)
-        fprintf(fid, '\t\t{');
         for jj=find(qx & qc)
-            fprintf(fid, '%s ', ar.model(m).x{jj});
+            fprintf(fid, '\t\t{%s;}\n', ar.model(m).x{jj});
         end
-        fprintf(fid, ';}\n');
     end
     
     % not measured species
     if(sum(~qx & qc)>0)
-        fprintf(fid, '\t\t{node [style=dashed]; ');
         for jj=find(~qx & qc)
-            fprintf(fid, '%s ', ar.model(m).x{jj});
+            fprintf(fid, '\t\t{node [style=dashed]; %s ;}\n', ar.model(m).x{jj});
         end
-        fprintf(fid, ';}\n');
     end
     
-    % fluxes
-    fprintf(fid, '\t\t{node [shape=box fontsize=10 width=0.1 height=0.1]; ');
+    % fluxes inside compartments
     for jf=1:size(ar.model(m).N,2)
         qsource = ar.model(m).N(:,jf) < 0;
         csource = ar.model(m).cLink(qsource);
@@ -105,26 +101,38 @@ for jc = 1:length(ar.model(m).c)
             error('different compartments for educts');
         end
         if(isempty(csource))
-            csource = ctarget;
+            if(ctarget == jc)
+                fprintf(fid, '\t\t{node [label="" shape=point]; source%i;}\n', jf);
+            end
+        elseif(isempty(ctarget))
             if(csource == jc)
-                fprintf(fid, '{node [label="" shape=point]; source%i;} ', jf);
+                fprintf(fid, '\t\t{node [label="" shape=point]; target%i;}\n', jf);
             end
         end
-        if(isempty(ctarget))
-            if(csource == jc)
-                fprintf(fid, '{node [label="" shape=point]; target%i;}', jf);
-            end
-        end
-        if(csource == jc)
-            fprintf(fid, 'v%i ', jf);
+        if((isempty(csource) && ctarget == jc) || (isempty(ctarget) && csource == jc) || (csource == jc && ctarget == jc))
+            fprintf(fid, '\t\t{node [shape=box fontsize=10 width=0.1 height=0.1]; v%i ;}\n', jf);
         end
     end
-    fprintf(fid, ';}\n');
     
 	fprintf(fid, '\t\tlabel="%s"\n', ar.model(m).c{jc});
+    
+    fprintf(fid, '\t}\n');
 end
-for jc = 1:length(ar.model(m).c)
-	fprintf(fid, '\t}\n');
+
+% fluxes outside compartments
+for jf=1:size(ar.model(m).N,2)
+    qsource = ar.model(m).N(:,jf) < 0;
+    csource = ar.model(m).cLink(qsource);
+    csource = unique(csource);
+    qtarget = ar.model(m).N(:,jf) > 0;
+    ctarget = ar.model(m).cLink(qtarget);
+    ctarget = unique(ctarget);
+    if(length(csource)>1)
+        error('different compartments for educts');
+    end
+    if(~isempty(csource) && ~isempty(ctarget) && csource ~= ctarget)
+        fprintf(fid, '\t\t{node [shape=box fontsize=10 width=0.1 height=0.1]; v%i ;}\n', jf);
+    end
 end
 
 % edges
@@ -139,7 +147,7 @@ for jj = 1:size(ar.model(m).N,2)
 				ar.model(m).x{source(j)}, jj, normweight);
 		end
 	else
-		fprintf(fid, '\t\t{node [label="" shape=point]; source%i;}\n', jj);
+% 		fprintf(fid, '\t\t{node [label="" shape=point]; source%i;}\n', jj);
 		fprintf(fid, '\t\tsource%i -> v%i [arrowhead=none weight=%i];\n', ...
 			jj, jj, normweight);
 	end
@@ -150,7 +158,7 @@ for jj = 1:size(ar.model(m).N,2)
 				jj, ar.model(m).x{target(j)}, normweight);
 		end
 	else
-		fprintf(fid, '\t\t{node [label="" shape=point]; target%i;}\n', jj);
+% 		fprintf(fid, '\t\t{node [label="" shape=point]; target%i;}\n', jj);
 		fprintf(fid, '\t\tv%i -> target%i [weight=%i];\n', jj, jj, normweight);
 	end
 end
