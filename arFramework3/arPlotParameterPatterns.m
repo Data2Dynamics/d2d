6e491c87e18edccf13d8a58f5a9c4be09c243903
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2015-04-03 09:32:27 -0400

    improved plotting

--- arFramework3/arPlotParameterPatterns.m
+++ arFramework3/arPlotParameterPatterns.m
@@ -12,13 +12,20 @@ end
 figure(1)
 C = jet(nbest);
 C = bsxfun(@rdivide, C, sqrt(sum(C.^2,2)));
+patch([ar.lb(jks) fliplr(ar.ub(jks))], [1:length(jks) length(jks):-1:1], ...
+    -1*ones(size([1:length(jks) length(jks):-1:1])), ...
+    'FaceColor', [.8 .8 .8], 'EdgeColor', 'none')
+hold on
 for j=1:nbest
     plot(ps(j,jks), 1:length(jks), 'Color', C(j,:));
-    hold on
 end
 hold off
-title(sprintf('parameter changes for %i best fits', nbest));
+title(sprintf('parameter differences between %i best fits', nbest));
 xlabel('parameter values')
 set(gca, 'YTick', 1:length(jks));
-set(gca, 'YTickLabel', arNameTrafo(ar.pLabel(jks)));
+set(gca, 'YTickLabel', cellfun(@(x,y) sprintf('%s #%i',x,y), ...
+    arNameTrafo(ar.pLabel(jks)), num2cell(jks), 'UniformOutput',false));
+set(gca, 'YDir','reverse');
 ylim([0 length(jks)+1])
+xrange = 0.01*(max(ar.ub(jks)) - min(ar.lb(jks)));
+xlim([min(ar.lb(jks))-xrange max(ar.ub(jks))+xrange])
