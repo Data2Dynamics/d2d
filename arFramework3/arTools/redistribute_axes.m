493b0c4c233693feb81042f99e1ab85b5df453da
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2015-07-28 11:57:06 -0400

    improved functionality

--- arFramework3/arTools/redistribute_axes.m
+++ arFramework3/arTools/redistribute_axes.m
@@ -1,4 +1,4 @@
-function h = redistribute_axes(h, n, m)
+function h = redistribute_axes(h, n, m, isort)
 
 hh = figure;
 for j = 1:(n*m)
@@ -6,12 +6,22 @@ for j = 1:(n*m)
     plot(rand(3));
 end
 
-ccount = 1;
+ichildren = [];
 for j=1:length(h.Children)
     if(isa(h.Children(j), 'matlab.graphics.axis.Axes') && ~isempty(h.Children(j).Children))
-        h.Children(j).Position = hh.Children(ccount).Position;
-        ccount = ccount + 1;
+        ichildren(end+1) = j; %#ok<AGROW>
     end
+end
+
+if(nargin<4)
+    isort = 1:length(ichildren);
+end
+
+ccount = 1;
+for j=1:length(ichildren)
+%     h.Children(ichildren(j)).Position = hh.Children(n*m - isort(ccount) + 1).Position;
+    h.Children(length(ichildren) - isort(ichildren(j)) + 1).Position = hh.Children(n*m - ccount + 1).Position;
+    ccount = ccount + 1;
 end
 
 close(hh);
