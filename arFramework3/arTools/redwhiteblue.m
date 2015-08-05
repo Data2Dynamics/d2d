1010bee22c12425bb8668af1b194343c9bcd4d3c
Max Schelker <m.schelker@gmx.com>
2015-08-05 14:08:22 -0400

    updates colormap

--- arFramework3/arTools/redwhiteblue.m
+++ arFramework3/arTools/redwhiteblue.m
@@ -1,10 +1,10 @@
 function cmap = redwhiteblue(m)
+% Interpolates Matlab's redbluecmap for a smooth color gradient from red
+% through white to blue.
 
 if nargin < 1, m = size(get(gcf,'colormap'),1); end
-n = floor(m/2);
-         
-r = [linspace(239,247,n) linspace(247,103,n)]';
-g = [linspace(138,247,n) linspace(247,169,n)]';
-b = [linspace(98,247,n) linspace(247,207,n)]';
 
-cmap = flipud([r g b]./247);
+tmp = redbluecmap(11);
+cmap(:,1) = interp1(linspace(0,1,size(tmp,1)), tmp(:,1), linspace(0,1,m));
+cmap(:,2) = interp1(linspace(0,1,size(tmp,1)), tmp(:,2), linspace(0,1,m));
+cmap(:,3) = interp1(linspace(0,1,size(tmp,1)), tmp(:,3), linspace(0,1,m));
