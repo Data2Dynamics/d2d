2c64a18558321c949985990b1f1488d19347f7f3
Clemens Kreutz <ckreutz@fdm.uni-freiburg.de>
2015-10-30 15:13:49 +0100

    Small change in the Setup of the DREAM Example.

--- arFramework3/Examples/Dream6/Setup.m
+++ arFramework3/Examples/Dream6/Setup.m
@@ -5,16 +5,18 @@ arInit;
 
 %% Choose on of the three models:
 
-% arLoadModel('model1_dream6');
-% arLoadData('all_model1', 1, 'xls',false);      % everything observed
+arLoadModel('model1_dream6');
+arLoadData('all_model1', 1, 'xls',false);      % everything observed
 
 % arLoadModel('model2_dream6');
 % arLoadData('all_model2', 1, 'xls',false);        % everything observed
 
-arLoadModel('model3_dream6');
-arLoadData('all_model3', 1, 'xls',false);      % everything observed
+% arLoadModel('model3_dream6');
+% arLoadData('all_model3', 1, 'xls',false);      % everything observed
 
 arCompileAll
+
+ar.config.fiterrors = 0;
 
 %% Loading of the original parameters and simulation of data:
 loadGoldStandardParameter(ar.model(1).name) % no priors
