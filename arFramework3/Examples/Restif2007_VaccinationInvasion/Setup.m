78b9def466c1f4830543a18af9104b66c79635ad
Clemens Kreutz <ckreutz@fdm.uni-freiburg.de>
2015-11-23 13:04:52 +0100

    New example (without data): Restif Vaccination

---
+++ arFramework3/Examples/Restif2007_VaccinationInvasion/Setup.m
@@ -0,0 +1,18 @@
+%% Create *.def
+arImportSBML('BIOMD0000000294',100)
+
+%% Load models & data
+arInit
+ar.config.checkForNegFluxes = false
+arLoadModel('BIOMD0000000294');
+arLoadData('BIOMD0000000294_data');
+arCompileAll;
+
+%% Check
+ar.config.atol=1e-10;
+ar.config.rtol=1e-10;
+arQplot('x')
+arPlot
+
+arCompareWithBiobaseSimulation('SIMU1448015036287.dat');
+
