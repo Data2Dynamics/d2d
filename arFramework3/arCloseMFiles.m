b64487ce3286befa53b20d98b4c8b742a4ac04bf
JoepVanlier <joep.vanlier@fdm.uni-freiburg.de>
2016-02-29 18:47:31 +0100

    make sure they don't throw errors

--- arFramework3/arCloseMFiles.m
+++ arFramework3/arCloseMFiles.m
@@ -2,4 +2,7 @@
 
 function arCloseMFiles
     % To do: Check whether this also works on other MATLAB versions
-    com.mathworks.mlservices.MLEditorServices.getEditorApplication.close
+    try
+        com.mathworks.mlservices.MLEditorServices.getEditorApplication.close
+    catch
+    end
--- arFramework3/arSetTitle.m
+++ arFramework3/arSetTitle.m
@@ -2,5 +2,8 @@
 
 function arSetTitle( title )
     % To do: Check whether this also works on other MATLAB versions
-    com.mathworks.mde.desk.MLDesktop.getInstance.getMainFrame.setTitle(title)
+    try
+        com.mathworks.mde.desk.MLDesktop.getInstance.getMainFrame.setTitle(title)
+    catch
+    end
     
