5004e6212190448b6098ec6e1185e1b91fced298
Max Schelker <max.schelker@biologie.hu-berlin.de>
2016-04-08 11:34:07 +0200

    adjusts path for SBML Test Suite

--- arFramework3/arSBMLTestSuite.m
+++ arFramework3/arSBMLTestSuite.m
@@ -2,11 +2,13 @@
 % Import and simulate all SBML Test Suite models (http://sbml.org/Facilities/Database) and
 % compare results with reference simulation.
 
-if(~exist('sbml-test-cases-2014-10-22','dir'))
+d2d_dir = fileparts(which('arInit.m'));
+
+if(~exist([d2d_dir '/sbml-test-cases-2014-10-22'],'dir'))
     fprintf(1, '\nDownloading SBML test cases...\n');
-    websave('sbml-test-cases-2014-10-22.zip','http://jaist.dl.sourceforge.net/project/sbml/test-suite/3.1.1/cases-archive/sbml-test-cases-2014-10-22.zip');
+    websave([d2d_dir '/sbml-test-cases-2014-10-22.zip'],'http://jaist.dl.sourceforge.net/project/sbml/test-suite/3.1.1/cases-archive/sbml-test-cases-2014-10-22.zip');
     fprintf(1, '\nUnizpping SBML test cases...\n');
-    unzip('sbml-test-cases-2014-10-22.zip','sbml-test-cases-2014-10-22');
+    unzip([d2d_dir '/sbml-test-cases-2014-10-22.zip'],[d2d_dir '/sbml-test-cases-2014-10-22']);
 end
 
 if ~exist('TestSuite','dir')
@@ -15,7 +17,7 @@ end
 
 cd('TestSuite')
 
-modeldir = '../sbml-test-cases-2014-10-22/cases/semantic/';
+modeldir = [d2d_dir '/sbml-test-cases-2014-10-22/cases/semantic/'];
 dirname = dir(modeldir);
 dirname = dirname([dirname.isdir]);
 dirname = dirname(3:end);
@@ -55,17 +57,19 @@ for jm = 1:length(dirname)
         arSimu(0,1,1);
 
         results = readtable([modeldir dirname(jm).name '/' modelnum '-results.csv'],'ReadVariableNames',true,'ReadRowNames',false);
+        varnames = results.Properties.VariableNames(2:end);
         
         % find variables and inputs (boundarySpecies)
-        xNames = intersect(results.Properties.VariableNames(2:end),ar.model.xNames);
-        uNames = intersect(results.Properties.VariableNames(2:end),ar.model.u);
+        xNames = intersect(varnames,ar.model.xNames);
+        uNames = intersect(varnames,ar.model.u);
         
         % interpolate time
         d2d_res = interp1(ar.model.condition.tFine,[ar.model.condition.xFineSimu ar.model.condition.uFineSimu],results.time);
         
         deviation = abs(results{:,[xNames uNames]}-d2d_res);
         results_table{modelnum,:} = {max(deviation(:)), []};
-        if all(all(deviation < 1e-5))
+        
+        if all(deviation(:)<1e-5))
             fprintf(1, '\n\nModel %s sucessfully imported! Maximal deviation: %2.2g\n\n', modelnum, max(deviation(:)));
         else
             warning('Maximal deviation >1e-5');
@@ -75,7 +79,6 @@ for jm = 1:length(dirname)
             plot(results.time,d2d_res,'o--')
             hold off
             title(modelnum)
-            varnames = results.Properties.VariableNames(2:end);
             legend([varnames varnames])
             pause    
         end
