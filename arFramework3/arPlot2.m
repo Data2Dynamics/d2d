e012101f308c5bfeead3ff6a3d543b27c93690fa
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2016-04-11 15:02:08 +0200

    Oops, forgot this is not C++ :)

--- arFramework3/arPlot2.m
+++ arFramework3/arPlot2.m
@@ -352,7 +352,7 @@ for jm = 1:length(ar.model)
                 if(isfield(ar.model(jm).plot(jplot), fighandel_name{jtype}))
                     if(sum(hs==ar.model(jm).plot(jplot).(fighandel_name{jtype}))==0)
                         try %#ok<TRYNC>
-                            if ( fastplot != 2 )
+                            if ( fastplot ~= 2 )
                                 close(ar.model(jm).plot(jplot).(fighandel_name{jtype}))
                             end
                         end
