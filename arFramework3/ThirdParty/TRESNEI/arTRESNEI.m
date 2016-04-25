7f19b6e91b902c55eb4f3b93f614698413e1cfb9
Franz-Georg Wieland <franz-georg.wieland@mars.uni-freiburg.de>
2016-04-25 13:27:14 +0200

    Minor clean up of TRESNEI code

--- arFramework3/ThirdParty/TRESNEI/arTRESNEI.m
+++ arFramework3/ThirdParty/TRESNEI/arTRESNEI.m
@@ -1,18 +1,22 @@
 % Wrapper for Tresnei function calling the TRESNEI.m in  with all relevant input arguments
-% 
-% Necessary changes in TRESNEI.m;
-% 
-% comment out: "sprintf('%s %s','Problem    = ', fun)" because sprintf 
+% (C) Franz-Georg Wieland 2016
+% Contact: franz-georg.wieland at mars.uni-freiburg.de 
+
+% Necessary changes in TRESNEI.m for implementation: 
+% comment out line 214: "sprintf('%s %s','Problem    = ', fun)" because sprintf 
 % can't deal with function handles
 
 
 function [p,exitflag,output,lambda,jac] = arTRESNEI(fun,x,l,u,arTRESoptions)
 
-% No complex constraints used in d2d thus dimensons of complex constraint
-% functions (e_i) are 0
+% TRESNEI allows for multidimensional constraint functions instead of
+% simple constant constraints, however no support for this kind of
+% constraint is implemented in D2D
+% (e_i) describes the dimensions of the non-constant constraint functions
+% --> set to 0 here
 e_i = [0,0];
 
-% Read out ar.optim.optimizer options to fit TRESNEI option structure
+% Read out ar.optim.optimizer options and fit them to TRESNEI option structure
 if isempty( arTRESoptions.MaxIter )
 else options.max_itns = arTRESoptions.MaxIter;
 end
@@ -34,11 +38,12 @@ end
 %                         .output<0   no output is displayed.
 %                         .output>0   one line of summary output for each iteration is 
 %                                     printed: 
-
 options.output = 0;
 
 
-% Call TRESNEI.m with relevant data
+% Call TRESNEI.m with relevant data, 
+% x l, u are transposed because TRESNEI.m uses column notation, opposed to row notation in D2D
+% Function handle is transposed for same reason
 [p,ierr,output_tresnei] = TRESNEI(x',e_i,@(pTrial)transposehandle(fun, pTrial),l',u',options);
 
 
@@ -52,11 +57,7 @@ output.funcCount = output_tresnei.feval;
 output.firstorderopt = output_tresnei.optimality;
 
 % set exitflag >0, because TRESNEI.m already prints its own output
-
 exitflag = 1;
-
-
-
 end
 
 
@@ -66,6 +67,7 @@ function [res,sres] = transposehandle(handle, pTrial)
     
 [res2,sres] = handle(pTrial);
 
+% Transpose first output argument (not jacobian)
 res = res2';
 end
 
