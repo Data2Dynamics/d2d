e62f50ed9456d25312e144a3a84178175212c326
Andreas Raue <andreas.raue@fdm.uni-freiburg.de>
2016-06-27 08:30:24 -0400

    Added functionality for sorting the list

--- arFramework3/arTools/countElements.m
+++ arFramework3/arTools/countElements.m
@@ -1,7 +1,25 @@
-function countElements(d)
+function countElements(d, sorted)
+
+if(~exist('sorted','var'))
+    sorted = false;
+end
 
 [ud, ~, jd] = unique(d);
 
+maxlength = max(cellfun(@length, d));
+
+counts = zeros(1,length(ud));
 for j=1:length(ud)
-    fprintf('%s\t: #%i\n', ud{j}, sum(jd==j));
+    counts(j) = sum(jd==j);
 end
+
+if(sorted)
+    [~, isort] = sort(counts);
+    counts = counts(isort);
+    ud = ud(isort);
+end
+
+for j=1:length(ud)
+    fprintf([sprintf('%%%is', maxlength) ' : %i\n'], ud{j}, counts(j));
+end
+
