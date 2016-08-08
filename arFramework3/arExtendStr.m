eed87a7b4e54fb2940e68f42d4b234a3845e9293
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2016-08-08 14:16:13 +0200

    Extended arExtendStr a little

--- arFramework3/arExtendStr.m
+++ arFramework3/arExtendStr.m
@@ -1,4 +1,29 @@
-function str = arExtendStr(str, n)
-nd = n - length(str);
-S = ' ';
-str = [str S(ones(1,nd))];
+% Extends a string to a specified length
+% 
+% function str = arExtendStr(str, n, formatting)
+%
+%   str         - String to extend
+%   n           - Number of characters to extend it to
+%   formatting  - Text alignment (optional):
+%                   'left' (default), 'right', 'center'
+%
+function str = arExtendStr(str, n, formatting)
+    if ~exist( 'formatting', 'var' )
+        formatting = 'left';
+    end
+    switch( formatting )
+        case 'left'
+            nd = n - length(str);
+            S = ' ';
+            str = [str S(ones(1,nd))];
+        case 'right'
+            nd = n - length(str);
+            S = ' ';
+            str = [S(ones(1,nd)) str];
+        case 'center'
+            nd = n - length(str);
+            nl = floor( nd / 2 );
+            nr = nd - nl;
+            S = ' ';            
+            str = [S(ones(1,nl)) str S(ones(1,nr))];
+    end
