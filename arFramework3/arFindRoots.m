ecf0c28b6997c16cdf9cb5dd6a3fa01f527a8f89
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2016-09-26 15:00:41 +0200

    Updated arFindRoots.m

--- arFramework3/arFindRoots.m
+++ arFramework3/arFindRoots.m
@@ -2,15 +2,20 @@
 
 function arFindRoots()
 
-    jm = 1;
-    jc = 2;
     global ar;
+    debug = 0;
+    jm = 1;
+    jc = 1;
+    tolerance = .01 * ar.config.eq_tol;
+    
+    condis = 'ss_condition';
     
     %%% Get a reference sensitivity for now
-    arCheckCache(1);
-    arSimu(true, true, true);
-    ar.model(jm).ss_condition(jc).sxFineSimu(end,:,:)
-    %%%
+    if ( debug )
+        arCheckCache(1);
+        arSimu(true, false, true);
+        ar.model(jm).ss_condition(jc).sxFineSimu(end,:,:)
+    end
     
     nS = length( ar.model(jm).x );
     
@@ -18,24 +23,30 @@ function arFindRoots()
     dependent = null(ar.model(jm).N.', 'rational');
     
     % Do not simulate, but grab initial condition that is set in the model
-    feval(ar.fkt, ar, true, false, true, false, 'ss_condition', 'ss_threads', 1);
+    ar.model(jm).(condis)(jc).dvdxNum = 0 * ar.model(jm).(condis)(jc).dvdxNum;
+    ar.model(jm).(condis)(jc).dvduNum = 0 * ar.model(jm).(condis)(jc).dvduNum;
+    ar.model(jm).(condis)(jc).dvdpNum = 0 * ar.model(jm).(condis)(jc).dvdpNum;
+    
+    feval(ar.fkt, ar, true, ar.config.useSensis, true, false, 'ss_condition', 'ss_threads', 1);
     x0 = ar.model(jm).ss_condition(jc).xFineSimu(1,:);
     
     % Set up the objective function for lsqnonlin
     fn = @(x)merit( x, jm, jc );
 
     % Estimate initials in steady state
-    opts = optimset('TolFun', 1e-8*1e-8, 'Display', 'Off' );
-    xnew = lsqnonlin( fn, x0, 0*x0, [], opts );
+    opts            = optimset('TolFun', tolerance*tolerance, 'Display', 'Off' );
+    [xnew resnorm]  = lsqnonlin( fn, x0, 0*x0, [], opts );
     
+    % Calculate sensitivities via implicit function theorem
+    dfdx = ar.model.N * ar.model(jm).(condis)(jc).dvdxNum;
+    dfdu = ar.model.N * ar.model(jm).(condis)(jc).dvduNum;
+    dfdp = ar.model.N * ar.model(jm).(condis)(jc).dvdpNum;
+    S = -inv(dfdx)*dfdp;
     
-    %feval(ar.fkt, ar, true, ar.config.useSensis && sensi, dynamics, false, 'ss_condition', 'ss_threads');
-        
-    % Calculate sensitivities via implicit function theorem (still something wrong)
-    dfdx = ar.model.N * ar.model(jm).condition(jc).dvdxNum;
-    dfdp = ar.model.N * ar.model(jm).condition(jc).dvdpNum;
-    S = -inv(dfdx)*dfdp
-
+    if ( resnorm > tolerance )
+        warning( 'Failure to converge when rootfinding for model %d, condition %d', jm, jc );
+    end
+    
     % Remove the override after determination
     ar.model(jm).ss_condition(jc).x0_override = [];
 end
@@ -45,6 +56,6 @@ function res = merit(x0, jm, jc)
     global ar;
     ar.model(jm).ss_condition(jc).x0_override = x0;
     
-    feval(ar.fkt, ar, true, false, true, false, 'ss_condition', 'ss_threads', 1);
+    feval(ar.fkt, ar, true, ar.config.useSensis, true, false, 'ss_condition', 'ss_threads', 1);
     res = ar.model(jm).ss_condition(jc).dxdt.*ar.model(jm).ss_condition(jc).dxdt;
 end
