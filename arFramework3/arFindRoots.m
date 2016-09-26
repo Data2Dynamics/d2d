3998f0be06be48199e44f914754262e053dca063
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2016-09-26 18:03:29 +0200

    updated arFindRoots (still not done)

--- arFramework3/arFindRoots.m
+++ arFramework3/arFindRoots.m
@@ -1,16 +1,39 @@
-% Work in progress... do not use yet!
+% Attempts to find initial conditions such that the rhs for a specific
+% condition equals the null vector.
+%
+%   [xnew, S] arFindRoots( jm, jc, condis )
+%
+% Usage:
+%       jm      - Model number
+%       jc      - Condition number
+%       condis  - Condition (either "condition" or "ss_condition")
+%
+% Returns:
+%       xnew    - Determined initial condition
+%       S       - Sensitivities at determined point
+%
+% Note: This is an internal function
 
-function arFindRoots()
+function [xnew, S] = arFindRoots(jm, jc, condis, useConserved)
 
     global ar;
     debug = 0;
-    jm = 1;
-    jc = 1;
     tolerance = .01 * ar.config.eq_tol;
     
-    condis = 'ss_condition';
+    if nargin < 1
+        jm = 1;
+    end
+    if nargin < 2
+        jc = 1;
+    end
+    if nargin < 3
+        condis = 'ss_condition';
+    end
+    if nargin < 4
+        useConserved = 1;
+    end
     
-    %%% Get a reference sensitivity for now
+    % Determine a reference sensitivity for debugging purposes
     if ( debug )
         arCheckCache(1);
         arSimu(true, false, true);
@@ -19,29 +42,26 @@ function arFindRoots()
     
     nS = length( ar.model(jm).x );
     
-    % Get conserved pools (need independent initials eventually)
-    dependent = null(ar.model(jm).N.', 'rational');
-    
-    % Do not simulate, but grab initial condition that is set in the model
-    ar.model(jm).(condis)(jc).dvdxNum = 0 * ar.model(jm).(condis)(jc).dvdxNum;
-    ar.model(jm).(condis)(jc).dvduNum = 0 * ar.model(jm).(condis)(jc).dvduNum;
-    ar.model(jm).(condis)(jc).dvdpNum = 0 * ar.model(jm).(condis)(jc).dvdpNum;
-    
+    % Grab initial x0 based on model parameters
     feval(ar.fkt, ar, true, ar.config.useSensis, true, false, 'ss_condition', 'ss_threads', 1);
-    x0 = ar.model(jm).ss_condition(jc).xFineSimu(1,:);
+    x0 = ar.model(jm).ss_condition(jc).xFineSimu(1,:);    
     
-    % Set up the objective function for lsqnonlin
-    fn = @(x)merit( x, jm, jc );
+    if ( useConserved )
+        % Get conserved pools (need independent initials eventually)
+        dependent = null(ar.model(jm).N.', 'rational');
+    
+        % Set up the objective function for lsqnonlin
+        fn = @(x)merit( x, jm, jc );
+    end
 
     % Estimate initials in steady state
     opts            = optimset('TolFun', tolerance*tolerance, 'Display', 'Off' );
-    [xnew resnorm]  = lsqnonlin( fn, x0, 0*x0, [], opts );
+    [xnew, resnorm] = lsqnonlin( fn, x0, 0*x0, [], opts );
     
     % Calculate sensitivities via implicit function theorem
     dfdx = ar.model.N * ar.model(jm).(condis)(jc).dvdxNum;
-    dfdu = ar.model.N * ar.model(jm).(condis)(jc).dvduNum;
     dfdp = ar.model.N * ar.model(jm).(condis)(jc).dvdpNum;
-    S = -inv(dfdx)*dfdp;
+    S    = -pinv(dfdx)*dfdp;
     
     if ( resnorm > tolerance )
         warning( 'Failure to converge when rootfinding for model %d, condition %d', jm, jc );
