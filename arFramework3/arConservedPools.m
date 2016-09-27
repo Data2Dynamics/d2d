9e7a823fa20e7b9c1f95e89b6b34d676b87c0bfb
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2016-09-27 18:26:01 +0200

    Account for compartment sizes in conserved pools

--- arFramework3/arConservedPools.m
+++ arFramework3/arConservedPools.m
@@ -6,16 +6,27 @@ function arConservedPools(jm, showPools)
         showPools = 0;
     end
     
-    N = ar.model(jm).N;
+    nx = length( ar.model(jm).x );
+    pc = cellfun(@str2num, ar.model.pc);
+    pc = pc(ar.model(jm).cLink);
+    N  = ar.model(jm).N;
+    
+    C = ones(nx, nx);
+    for js1 = 1 : nx
+        for js2 = 1 : nx
+            C(js1,js2) = C(js1,js2) * (pc(js1)/pc(js2));
+        end
+    end
+    
     dependent = null( N.', 'r' );
     
     IDs = repmat(1:size(dependent,1).', size(dependent,2), 1).';
     groups = zeros( size(IDs) );
     
-    selected = dependent;
+    selected = dependent .* repmat(pc.', 1, size(dependent,2))
     states = zeros( 1, size( dependent, 2 ) );
     for js = 1 : size( dependent, 2 )
-        removedState = find( selected == 1, 1 );
+        removedState = find( abs(selected) > 0, 1 );
         curCoeff = selected(removedState, 1);
         states(js) = IDs(removedState,1);
         IDs(removedState, :) = [];
@@ -34,21 +45,22 @@ function arConservedPools(jm, showPools)
     end
     
     % Mapping from reduced model states to full states
-    mapping = eye(length(ar.model.x));    
+    mapping = eye(nx);    
     for js = 1 : length( states )
         mapping(states(js),:) = groups(:,js);
     end
     mapping(:, states) = [];
     
     % Matrix to compute totals for respective species
-    totalMapping = zeros(length(ar.model.x));
+    totalMapping = zeros(nx);
     totalMapping(states, :) = dependent.';
     
-    ar.model(jm).pools.dependent = dependent;
-    ar.model(jm).pools.others    = groups;
-    ar.model(jm).pools.states    = states;
-    ar.model(jm).pools.mapping   = mapping;
-    ar.model(jm).pools.totalMap  = totalMapping;
+    ar.model(jm).pools.dependent  = dependent;
+    ar.model(jm).pools.others     = groups;
+    ar.model(jm).pools.states     = states;
+    ar.model(jm).pools.usedStates = setdiff( 1:length(ar.model(jm).x), states );
+    ar.model(jm).pools.mapping    = mapping;
+    ar.model(jm).pools.totalMap   = totalMapping;
     
     if ( showPools )
         disp( 'Reparameterized model:' );
@@ -56,9 +68,9 @@ function arConservedPools(jm, showPools)
             c = '';
             for js = 1 : size( groups, 1 )
                 if ( abs( groups(js, jg) ) > 0 )
-                    c = sprintf( '%s + %d %s ', c, groups(js, jg), ar.model.x{js} );
+                    c = sprintf( '%s + %d %s ', c, groups(js, jg), ar.model(jm).x{js} );
                 end
             end
-            fprintf( '%s = total_%d %s\n', ar.model.x{states(jg)}, jg, c );
+            fprintf( '%s = total_%d %s\n', ar.model(jm).x{states(jg)}, jg, c );
         end
     end
