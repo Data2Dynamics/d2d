455953ec22dd65277a84e17b3b593b7556a072bb
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2016-09-27 15:25:43 +0200

    helper function to compute conserved pools

---
+++ arFramework3/arConservedPools.m
@@ -0,0 +1,50 @@
+function arConservedPools(jm, showPools)
+    
+    global ar;
+    
+    if ( nargin < 2 )
+        showPools = 0;
+    end
+    
+    N = ar.model(jm).N;
+    dependent = null( N.', 'r' );
+    
+    IDs = repmat(1:size(dependent,1).', size(dependent,2), 1).';
+    groups = zeros( size(IDs) );
+    
+    selected = dependent;
+    for js = 1 : size( dependent, 2 )
+        removedState = find( selected == 1, 1 );
+        curCoeff = selected(removedState, 1);
+        states(js) = IDs(removedState,1);
+        IDs(removedState, :) = [];
+        selected(removedState, :) = [];
+        
+        % List of other species involved in this relation
+        otherSpecies = IDs(:,1);
+        otherSpecies = otherSpecies(  abs( selected(:,1) ) > 0 );
+        stoich       = selected(  abs( selected(:,1) ) > 0 );
+        
+        % Store coefficients of other species involved in the conserved pool
+        groups( otherSpecies, js ) = - stoich / curCoeff;
+        
+        IDs(:, 1) = [];
+        selected(:, 1) = [];
+    end
+    
+    ar.model(jm).pools.dependent = dependent;
+    ar.model(jm).pools.others    = groups;
+    ar.model(jm).pools.states    = states;
+    
+    if ( showPools )
+        disp( 'Reparameterized model:' );
+        for jg = 1 : size( groups, 2 )
+            c = '';
+            for js = 1 : size( groups, 1 )
+                if ( abs( groups(js, jg) ) > 0 )
+                    c = sprintf( '%s + %d %s ', c, groups(js, jg), ar.model.x{js} );
+                end
+            end
+            fprintf( '%s = total_%d %s\n', ar.model.x{states(jg)}, jg, c );
+        end
+    end
