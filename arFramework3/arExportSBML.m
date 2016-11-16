2446c417d983ce0458dd249c1928f6fbbc255d73
Max Schelker <max.schelker@hu-berlin.de>
2016-11-16 10:14:28 +0100

    small fix in SBML exporter that occurred when having condition specific initial values. Please check if I broke anything!

--- arFramework3/arExportSBML.m
+++ arFramework3/arExportSBML.m
@@ -121,11 +121,13 @@ for jx = 1:length(ar.model(m).x)
     M.species(jx).version = 4;
     
     qp = ismember(ar.pLabel, ar.model(m).px0{jx}); %R2013a compatible
-    if(sum(qp)==1)
+    % check if init parameter still exists in condition parameters
+    is_set_cond = sum(ismember(ar.model(m).condition(c).fp, ar.model(m).px0{jx}))==0;
+    if(sum(qp)==1 && ~is_set_cond)
         M.species(jx).initialConcentration = 1;
         Crules{end+1,1} = ar.model(m).x{jx}; %#ok<AGROW>
         Crules{end,2} = ar.pLabel{qp}; %#ok<AGROW>
-    elseif(sum(qp)==0)
+    elseif(sum(qp)==0 || is_set_cond)
         qp = ismember(ar.model(m).condition(c).pold, ar.model(m).px0{jx}); %R2013a compatible
         if(sum(qp)==1)
             pvalue = char(sym(ar.model(m).condition(c).fp{qp}));
