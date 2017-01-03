32649ba5fa6e84466297c5846f519796bf226e9f
Clemens Kreutz <ckreutz@fdm.uni-freiburg.de>
2017-01-04 00:04:15 +0100

    Warning if field is missing

--- arFramework3/Advanced/arPlotRandomEffects.m
+++ arFramework3/Advanced/arPlotRandomEffects.m
@@ -4,14 +4,21 @@ function arPlotRandomEffects
 
 global ar
 
-figure(1);
-
-[nrows, ncols] = arNtoColsAndRows(length(ar.random));
-
-for j = 1:length(ar.random)
-    subplot(nrows, ncols, j);
+if isfield(ar,'random')
     
-    hist(ar.p(ar.random{j}), 50);
+    figure(1);
     
-    title(strrep(ar.pLabel(ar.random{j}(1)),'_','\_'));
+    [nrows, ncols] = arNtoColsAndRows(length(ar.random));
+    
+    for j = 1:length(ar.random)
+        subplot(nrows, ncols, j);
+        
+        hist(ar.p(ar.random{j}), 50);
+        
+        title(strrep(ar.pLabel(ar.random{j}(1)),'_','\_'));
+    end
+else
+    warning('No random effects defined.')
 end
+
+    
