4d0cf8bb7c081373dad3f54cb88b9b48da571d24
Andreas Raue <andreas.raue@me.de>
2017-02-13 20:12:46 -0500

    now allows for multiple pattens.

--- arFramework3/MatlabTools/strfindcell.m
+++ arFramework3/MatlabTools/strfindcell.m
@@ -1,3 +1,11 @@
 function q = strfindcell(data, pattern)
 
+if(iscell(pattern))
+    q = false(size(data));
+    for j=1:length(pattern)
+        q = q | strfindcell(data, pattern{j});
+    end
+    return
+end
+
 q = ~cellfun(@isempty, cellfun(@strfind, data, repmat({pattern},size(data,1),size(data,2)), 'UniformOutput', false));
