f5b7779a058dc6243e6344ab22b7546c594f4d1c
Helge Hass <helge.hass@fdm.uni-freiburg.de>
2017-02-16 14:23:01 +0100

    functionality to couple integrator and optimization tolerances

--- arFramework3/Subfunctions/arImportPars.m
+++ arFramework3/Subfunctions/arImportPars.m
@@ -133,6 +133,8 @@ nnot = length(pStruct.p)-sum(ass);
 if ( nnot > 0 )
     arFprintf(1, 'There were %i more parameters in the loaded struct than in the target model.\n',nnot);
 end
+tmp=ismember(pStruct.pLabel, ar.pLabel);
+arFprintf(1, 'Not assigned from loaded struct are: %s \n',sprintf('%s, ',pStruct.pLabel{~tmp}));
 
 if nargout>0
     varargout{1} = ar;
