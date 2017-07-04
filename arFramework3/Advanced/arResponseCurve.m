73d421c9998bcc39a249a741491b6b9ddf07c9f2
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2017-07-04 16:01:25 +0200

    Added relative option for response curves

--- arFramework3/Advanced/arResponseCurve.m
+++ arFramework3/Advanced/arResponseCurve.m
@@ -1,35 +1,58 @@
 % Show rate response curve w.r.t. two variables of a model.
 %
 % Usage:
-%		arResponseCurve( name, indep1, indep2, timepoints )
+%		arResponseCurve( name, indep1, indep2, varargs )
 %
 % Arguments:
 % 		name 			- Name of the flux
-%		indep1		- Independent variable 1 (x-axis)
-% 		indep2 		- Independent variable 2
-%		timepoints 	- Simulation time point to use for values of the other states (default is initial condition)
+%		indep1          - Independent variable 1 (x-axis)
+% 		indep2          - Independent variable 2
+% Variable arguments:
+%		timepoints      - Simulation time point to use for values of the other states (default is initial condition)
+%		model           - Which model to use for reference species
+%		condition       - Which model condition to use for reference species
+%		relative        - Divide rate by rate which is obtained by taking
+%                         the limit of the substrates -> infinity and 
+%                         products -> 0 (for enzymes this'd typically be vmax)
+%       
 %
 % NOTE: THIS FUNCTION IS CURRENTLY IN ALPHA STATUS!
 % PLEASE DO NOT EDIT THIS FUNCTION YET. IT IS STILL UNDER ACTIVE DEVELOPMENT.
 % FUNCTION ARGUMENTS ARE LIKELY SUBJECT TO CHANGE IN THE NEAR FUTURE.
-function rate = arResponseCurve( name, indep1, indep2, timepoints )
+function rate = arResponseCurve( name, indep1, indep2, varargin )
 
     global ar;
     
     ylog = 0;
     m = 1;
     cond = 1;
+    timepoints = 1;
     miniTresh = 1e-16;
-    if ~exist( 'timepoints', 'var' )
-        timepoints = 1;
+    
+    args = {'timepoints', 'condition', 'model', 'relative'};
+    extraArgs = [1, 1, 1, 0];
+    opts = argSwitch( args, extraArgs, {}, 0, varargin );
+
+    if opts.timepoints
+        timepoints = opts.timepoints_args;
+    end
+    if opts.condition
+        cond = opts.condition_args;
+    end
+    if opts.model
+        m = opts.model_args;
     end
     
-    figure; hold on;
+    %figure;
+    hold on;
     NX = ceil(sqrt(numel(timepoints)));
     NY = ceil(numel(timepoints)/NX);
+    arSimu(false, true, true);
     for ti = 1 : numel( timepoints )
         
-        subplot(NX, NY, ti); hold on;
+        if ( numel( timepoints ) > 1 )
+            subplot(NX, NY, ti); hold on;
+        end
         tp = timepoints(ti);
         
         enzyme = ismember( ar.model.v, name );
@@ -39,21 +62,43 @@ function rate = arResponseCurve( name, indep1, indep2, timepoints )
         xLabels = ar.model.x;
         zLabels = ar.model.z;
         uLabels = ar.model.u;
+        
         pValues = arGetPars( ar.pLabel, 0 );
-        xValues = ar.model(m).condition(cond).xFineSimu(tp, :);
-        zValues = ar.model(m).condition(cond).zFineSimu(tp, :);
-        uValues = ar.model(m).condition(cond).uFineSimu(tp, :);
+        xValues = ar.model(m).condition(cond).xFineSimu(tp, :) + 0;
+        zValues = ar.model(m).condition(cond).zFineSimu(tp, :) + 0;
+        uValues = ar.model(m).condition(cond).uFineSimu(tp, :) + 0;
         
         labels = [ pLabels, xLabels, zLabels, uLabels ];
         values = [ pValues, xValues, zValues, uValues ];      
-
-        [~, ~, Iref] = intersect( labels, {indep1, indep2} );
+        
+        [~, Iref, ~] = intersect( labels, {indep1, indep2} );
         refValues = values(Iref);
-
+        
         % Remove the variables we want to scan
         [labels, I] = setdiff( labels, {indep1, indep2} );
         values = values(I);
 
+        if ( opts.relative )
+            % Get the vmax of this reaction
+            substrates = ar.model.fv_source{enzyme};
+            products = ar.model.fv_target{enzyme};
+            products = setdiff( products, substrates );
+            limF = func;
+            for i = 1 : numel( substrates )
+                limF = limit( limF, substrates{i}, inf );
+            end
+            for i = 1 : numel( products )
+                limF = limit( limF, products{i}, 0 );
+            end
+            
+            % Normalize by vmax
+            func = func / limF;
+            
+            rateName = sprintf( 'v_{%s} / v_{%s}^{max}', ar.model.v{enzyme}, ar.model.v{enzyme} );
+        else
+            rateName = sprintf( 'v_{%s}', ar.model.v{enzyme} );
+        end
+        
         % Substitute variables
         func = subs( func, labels, values );
 
@@ -98,11 +143,19 @@ function rate = arResponseCurve( name, indep1, indep2, timepoints )
         set( gca, 'XScale', 'log' )
         if ( ylog )
             set( gca, 'YScale', 'log' )
+            ylabel( sprintf( 'log_{10}(%s)', rateName ) );
+        else
+            ylabel( sprintf( '%s', rateName ) );
         end
+        nX = 5;
+        set( gca, 'XTick', 10.^[min(log10(responseRange1)) : (max(log10(responseRange1)) - min(log10(responseRange1))) / nX : max(log10(responseRange1))] );
         set( gca, 'CLim', [min(log10(responseRange2)), max(log10(responseRange2))] );
         c = colorbar;
         colormap(parula);
         ylabel( c, sprintf( 'log_{10}(%s)', indep2 ) );
     
+        if ( nargout == 0 )
+            clear rate;
+        end
     end
 end
