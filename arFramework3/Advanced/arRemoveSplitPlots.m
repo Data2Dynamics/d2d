b47858019f9c08686adf9e80bdd09a469477e10b
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2017-07-18 11:18:06 +0200

    Small helper function to quickly clean up plots that were generated with arSplitPlot

---
+++ arFramework3/Advanced/arRemoveSplitPlots.m
@@ -0,0 +1,20 @@
+% Removes all run time generated plots (i.e. generated with arSplitPlot) from the model
+%
+function arRemoveSplitPlots()
+    global ar;
+    
+    for m = 1 : numel( ar.model )
+        jps = [];
+        for jp = 1 : numel( ar.model(m).plot )
+            if ( isfield( ar.model(m).plot(jp), 'origin' ) && ~isempty( ar.model(m).plot(jp).origin ) )
+                jps = [ jps, jp ];
+            end
+        end
+        if ( numel( jps ) > 0 )
+            arRemovePlot( m, jps );
+            fprintf( 'Removed %d plots from model %d\n', numel( jps ), m );
+        else
+            fprintf( 'No plots were removed from model %d\n', m );
+        end
+    end
+end
