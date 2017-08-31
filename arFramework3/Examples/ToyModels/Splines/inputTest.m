6e773cfeddb82a576241b2dbc067edc67f23e52d
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2017-08-31 11:10:52 +0200

    Added example for a really long spline input

--- arFramework3/Examples/ToyModels/Splines/inputTest.m
+++ arFramework3/Examples/ToyModels/Splines/inputTest.m
@@ -1,7 +1,19 @@
+% This file shows an example of how to use fixed input splines. These
+% splines cannot take free parameters, but they can be used to import
+% input functions with a large number of time points.
+
 arInit;
 arLoadModel('input');
-arLoadData('test', 1, 'csv');
+arLoadData('long', 1, 'csv');
 arCompileAll(true);
-%arFit;
-%arPlot;
+
+% Make sure that we actually see every data point (otherwise it seems that
+% not all the data is fitted, because the displayed points do not end up at
+% the spline knots.
+ar.model.data.tExtra = [1:5000]; arLink;
+
+% We should really use the new plots because the performance of the old
+% ones is terrible when having so many points.
+ar.config.useNewPlots=1;
+arPlot;
 title('Monotone spline (monotone between spline knots)');
