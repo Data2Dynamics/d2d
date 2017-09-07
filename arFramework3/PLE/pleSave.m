2fa35971467d7617ab9903d76a6b293e4a5cee2e
Christian Tönsing <christian.toensing@fdm.uni-freiburg.de>
2017-09-07 16:16:23 +0200

    small bugfix in pleSave. The function is still in use by ple.m to ensure a backup after each profile in PLE/results.mat and it should not be in the 'Deprecated' folder.

--- arFramework3/PLE/pleSave.m
+++ arFramework3/PLE/pleSave.m
@@ -5,10 +5,10 @@
 %   Saves the input variable ar.ple to ar.ple.savePath
 %   in workspace result.mat
 % 
-%   This funciton doesn't use the  to
+%   This funciton doesn't use the info in global ar to
 %   prevent that manipulations have effect on the global variable 
 % 
-% 
+% s
 % 
 
 function pleSave(ple)
@@ -16,7 +16,7 @@ if(~exist([cd '/' ple.savePath], 'dir'))
     mkdir([cd '/' ple.savePath])
 end
 
-warning('pleSave is deprecated. pleGlobals is now stored as ar.ple by using arSave')
+%warning('pleSave is deprecated. pleGlobals is now stored as ar.ple by using arSave')
 
 ple.fighandel_multi = [];    % remove handle to
 save([ple.savePath '/results.mat'], 'ple');
