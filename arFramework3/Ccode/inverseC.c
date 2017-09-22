cbf9a28b40b1d9cfc40d40c218fe1724cae7dcd0
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2017-09-22 16:36:33 +0200

    Minor optimization

--- arFramework3/Ccode/inverseC.c
+++ arFramework3/Ccode/inverseC.c
@@ -9,21 +9,12 @@
 #define DEBUGPRINT4(DBGMODE, LVL, STR, ARG1, ARG2, ARG3, ARG4) { if ( DBGMODE > LVL ) { mexPrintf( " [D] " ); mexPrintf( STR, ARG1, ARG2, ARG3, ARG4 ); mexEvalString("drawnow;"); } }
 
 /* Invert a matrix using LAPACK */
-void invert(double* mat, mwSignedIndex N)
+void invert(double* mat, mwSignedIndex workSize, double *workmem, mwSignedIndex *ipiv, mwSignedIndex N)
 {
-    mwSignedIndex *ipiv;
-    double *workmem;
-    mwSignedIndex workSize = N*N;
     mwSignedIndex errorCode;
     
-    ipiv = (mwSignedIndex *) malloc(sizeof(mwSignedIndex) * (N+1));
-    workmem = (double *) malloc( sizeof(double) * workSize );
-
     dgetrf(&N,&N,mat,&N,ipiv,&errorCode);
     dgetri(&N,mat,&N,ipiv,workmem,&workSize,&errorCode);
-
-    free(ipiv);
-    free(workmem);
 };
 
 /* Matrix multiply for the rootfinding using BLAS */
@@ -39,12 +30,12 @@ void mmultiply(double* invdfdx, double* f, double *result, mwSignedIndex N)
 };
 
 /* Single Newton-Raphson step */
-double NRstep( double *xptr, double* dfdx, double* f, double *tmp, int N )
+double NRstep( double *xptr, double* dfdx, double* f, double *tmp, mwSignedIndex workSize, double *workmem, mwSignedIndex *ipiv, int N )
 {
     double maxAbs;
     int i;
            
-    invert(dfdx, N);
+    invert(dfdx, workSize, workmem, ipiv, N);
     mmultiply( dfdx, f, tmp, N );
 
     maxAbs = 0.0;
@@ -68,40 +59,38 @@ double solveSS( int debugMode, mxArray *arcondition, int im, int ic, int isim, d
 	double *dfdx;
     double *f;
     double *tmp;
-    
+    mwSignedIndex *ipiv;
+    double *workmem;
+    mwSignedIndex workSize;
+       
     xptr            = N_VGetArrayPointer(x);
     dfdx            = mxGetData(mxGetField(arcondition, ic, "dfdxNum"));
     f               = mxGetData(mxGetField(arcondition, ic, "dxdt"));
     N               = mxGetNumberOfElements(mxGetField(arcondition, ic, "dxdt"));
-    tmp             = (double *)malloc( sizeof( double ) * N );
+    workSize        = N*N;
     iterationLimit  = 100;
     maxAbs          = 100000000.0;
+    
+    /* Allocate temporary storage for calculations */
+    tmp = (double *)malloc( sizeof( double ) * N );
+    ipiv = (mwSignedIndex *) malloc( sizeof(mwSignedIndex) * (N+1) );
+    workmem = (double *) malloc( sizeof(double) * workSize );    
     
     /* Perform Newton Raphson until convergence or abort */
     i = 0;
     while( ( maxAbs > tol ) && ( i < iterationLimit ) )
     {
         DEBUGPRINT3( debugMode, 9, "Rootfinding iteration %d: Maximum absolute dx: %g (tolerance = %g)\n", i, maxAbs, tol );
-        fx( t, x, f, user_data, im, isim );                           /* Compute RHS and store in f */
-        if ( debugMode > 10 )
-        {
-            int j;
-            for ( j = 0; j < N; j++ )
-                DEBUGPRINT2( debugMode, 10, "fx[%d] = %g", j, f[j] );
-            
-            for ( j = 0; j < N; j++ )
-                DEBUGPRINT2( debugMode, 10, "x[%d] = %g", j, x[j] );            
-        }
-        DEBUGPRINT0( debugMode, 10, "postfx\n" );
-        getdfxdx( im, isim, t, x, dfdx, user_data );                  /* Updates dvdx and stores dfxdx */
-        DEBUGPRINT0( debugMode, 10, "postgetdfdx\n" );
-        maxAbs = NRstep( xptr, dfdx, f, tmp, N );                     /* Perform Newton-Raphson step */
-        DEBUGPRINT0( debugMode, 10, "postNRstep\n" );
+        fx( t, x, f, user_data, im, isim );                                 /* Compute RHS and store in f */
+        getdfxdx( im, isim, t, x, dfdx, user_data );                        /* Updates dvdx and stores dfxdx */
+        maxAbs = NRstep( xptr, dfdx, f, tmp, workSize, workmem, ipiv, N );  /* Perform Newton-Raphson step */
         i++;
     }
     DEBUGPRINT3( debugMode, 9, "Final absolute at interation %d = dx: %g (tolerance = %g)\n", i, maxAbs, tol );
     
-    free( tmp );
+    free(tmp);
+    free(ipiv);
+    free(workmem);
     
     return maxAbs;
 }
--- arFramework3/Ccode/inverseC.h
+++ arFramework3/Ccode/inverseC.h
@@ -10,10 +10,10 @@
 double solveSS( int debugMode, mxArray *arcondition, int im, int ic, int isim, double t, N_Vector x, void *user_data, double tol );
 
 /* Single Newton-Raphson step */
-double NRstep( double *xptr, double* dfdx, double* f, double *tmp, int N );
+double NRstep( double *xptr, double* dfdx, double* f, double *tmp, mwSignedIndex workSize, double *workmem, mwSignedIndex *ipiv, int N );
 
 /* Invert a matrix using LAPACK */
-void invert(double* mat, mwSignedIndex N);
+void invert(double* mat, mwSignedIndex workSize, double *workmem, mwSignedIndex *ipiv, mwSignedIndex N);
 
 /* Matrix multiply for the rootfinding using BLAS */
 void mmultiply(double* invdfdx, double* f, double *result, mwSignedIndex N);
