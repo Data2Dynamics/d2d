15fa5c8de0f99e177ab408e8cf98aba6abb6a2d6
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2017-10-10 13:16:28 +0200

    Added function which cleans resident mex files (this helps prevent slowdown of your MATLAB sessions when you often compile or recompile models)

---
+++ arFramework3/Subfunctions/arCleanMemory.m
@@ -0,0 +1,29 @@
+% Mex files can sometimes leave a huge amount of memory resident.
+% This function removes the ones we keep resident, when possible.
+function arCleanMemory(clearallcompiled)
+
+    [~, loaded_mexfiles] = inmem('-completenames');
+    if ( nargin < 1 )
+        clearallcompiled = 0;
+    end
+
+    fprintf( 'Clearing old D2D mex files from memory ' );
+    for jm = 1 : numel( loaded_mexfiles )
+        [direc, mexName] = fileparts(loaded_mexfiles{jm});
+
+        % Is it one of our files? Then clear it from memory!
+        if ( ~isempty( strfind(mexName, 'arSimuCalcFun') ) || (clearallcompiled) )
+            try
+                fprintf( '.' );
+                oldDir = pwd;
+                cd( direc );
+                clear( mexName );
+                cd( oldDir );
+            catch
+                warning( 'Could not remove loaded mex file (file locked or directory deleted?)' );
+            end
+        end
+    end
+    fprintf( ' [OK]\n' );
+    
+end
--- arFramework3/arInit.m
+++ arFramework3/arInit.m
@@ -4,6 +4,7 @@
 % Website: http://www.data2dynamics.org
 % Contact: Andreas Raue - andreas.raue@fdm.uni-freiburg.de
 % Copyright 2016 D2D Development Team. All rights reserved.
+arCleanMemory;
 ar_path = fileparts(which('arInit.m'));
 if(exist('arCheck','file') == 0)
     addpath([ar_path '/Subfunctions'])
