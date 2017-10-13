ea069e5f412f8f1766032f141b7e939e200cb720
Helge Hass <helge.hass@fdm.uni-freiburg.de>
2017-10-13 16:56:32 +0200

    simplifications and commentation of BDT pred in example

--- arFramework3/Examples/Hass_npjSysBio2017/PanRTK_BDT_Computation/plot_BDT_einzel_RNAvsModel.m
+++ arFramework3/Examples/Hass_npjSysBio2017/PanRTK_BDT_Computation/plot_BDT_einzel_RNAvsModel.m
@@ -7,8 +7,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Go through specified ligands and extract their prediction efficiency,
-% F-measure and plot boxplots of TP/FP ratio as well as histograms and
-% cumulative densitiy curves for the n training runs wrt naive growth
+% F-measure and plot histograms and
+% cumulative densitiy curves for the n training runs wrt random prediction
 
 function efficiency = plot_BDT_einzel_RNAvsModel(dosave,perc)
 global bdt_figures
@@ -25,39 +25,38 @@ if(dosave)
     end
 end
 
-list_ligands = {'EGF','HRG','HGF','ALL'}; %'IGF1','HRG/IGF1'
+list_ligands = {'EGF','HRG','HGF','ALL'};%{'ALL'};%
 BDT_plots = struct();
 efficiency = NaN(2,length(list_ligands));
 
 for i = 1:length(list_ligands)
     
+    %Determine name of saved struct
     if(strcmp(list_ligands{i},'ALL'))
         lig_str = ['mut_wHGF_0_' num2str(perc)];
     else
         lig_str = [list_ligands{i} '_resp_0_' num2str(perc)];
     end
-    figure
+    
+    %Calculate MPV = (PPV+NPV)/2 for both RNA and model based predictions
     efficiency(2,i) = plot_Conf(['RNA_' lig_str],save_folder,list_ligands{i});
-    figure
     efficiency(1,i) = plot_Conf(['MODEL_' lig_str],save_folder,list_ligands{i});
-%     BDT_plots.(['RNA_' list_ligands{i}]) = bdt_figures.(['RNA_' lig_str])(1).testing(2).TFP(:,1,3);
-%     BDT_plots.(['MODEL_' list_ligands{i}]) = bdt_figures.(['MODEL_' lig_str])(1).testing(2).TFP(:,1,3);
-    BDT_plots.(['RNA_' list_ligands{i}]) = (bdt_figures.(['RNA_' lig_str])(1).testing(2).TFP(:,1,3) + bdt_figures.(['RNA_' lig_str])(1).testing(2).TFP(:,1,2))./2;
-    BDT_plots.(['MODEL_' list_ligands{i}]) = (bdt_figures.(['MODEL_' lig_str])(1).testing(2).TFP(:,1,3) + bdt_figures.(['MODEL_' lig_str])(1).testing(2).TFP(:,1,2))./2;
+    
+    %Store MPV of all trained BDTs in BDT_plots struct
+    BDT_plots.(['RNA_' list_ligands{i}]) = (bdt_figures.(['RNA_' lig_str])(1).testing(1).MPV(:,1,1) + bdt_figures.(['RNA_' lig_str])(1).testing(1).MPV(:,1,2))./2;
+    BDT_plots.(['MODEL_' list_ligands{i}]) = (bdt_figures.(['MODEL_' lig_str])(1).testing(1).MPV(:,1,1) + bdt_figures.(['MODEL_' lig_str])(1).testing(1).MPV(:,1,2))./2;
     hold off
     for j=1:length(bdt_figures.(['MODEL_' lig_str]))
-%         BDT_plots.(['MODEL_' list_ligands{i}])(j) = BDT_plots.(['MODEL_' list_ligands{i}])(j) - bdt_figures.(['MODEL_' lig_str])(j).naive_growth;
-%         BDT_plots.(['RNA_' list_ligands{i}])(j) = BDT_plots.(['RNA_' list_ligands{i}])(j) - bdt_figures.(['RNA_' lig_str])(j).naive_growth;       
+        %-0.5 to get it distributed around random prediction
         BDT_plots.(['MODEL_' list_ligands{i}])(j) = BDT_plots.(['MODEL_' list_ligands{i}])(j) - .5;
         BDT_plots.(['RNA_' list_ligands{i}])(j) = BDT_plots.(['RNA_' list_ligands{i}])(j) - .5;       
     end
-        
+    %Histogram plot of both MPV against random    
     figure
-    histogram(BDT_plots.(['MODEL_' list_ligands{i}]),15)
+    histogram(BDT_plots.(['MODEL_' list_ligands{i}]),[-0.4:0.05:0.5])%,15)%
     hold on
-    histogram(BDT_plots.(['RNA_' list_ligands{i}]),15)
+    histogram(BDT_plots.(['RNA_' list_ligands{i}]),[-0.4:0.05:0.5])%,15)%
     set(gcf,'Color','w')
-    % set(gca,'YLim',[0 100])
     xlabel('Difference BDT prediction to random [%]')
     ylabel('count')
     legend({'MODEL','RNA Expr'})
@@ -66,7 +65,7 @@ for i = 1:length(list_ligands)
         saveas(gcf,[save_folder 'BDT_hist_' list_ligands{i} '.fig'])
         saveas(gcf,[save_folder 'BDT_hist_' list_ligands{i}],'svg')        
     end
-    
+    %Cumulative density plot of both MPV against random
     figure
     cdfplot(BDT_plots.(['MODEL_' list_ligands{i}]))
     hold on
--- arFramework3/Examples/Hass_npjSysBio2017/PanRTK_BDT_Computation/plot_Conf.m
+++ arFramework3/Examples/Hass_npjSysBio2017/PanRTK_BDT_Computation/plot_Conf.m
@@ -12,8 +12,25 @@
 % folder = output folder for the figures, if blank, BDT_plots is used
 
 % which_lig = which ligand to be plotted, same as for BDT_bootstrap
+% MPV = mean predictive value, the measure we look at (PPV+NPV)/2
+% The matrix in this script has MPV = [NPV_BDT1 PPV_BDT1]
+%                                     [NPV_BDT2 PPV_BDT2]
+%                                     [...      ...     ]
+%
+% ConfusionMatrix as given from BDT_getROC (0 = no growth, 1 = growth)
+% ROWS=TRUE VALUES / COLS= PRED VALUES
+%    0    1
+% 0 TN   FP
+% 1 FN   TP
+%
+% For each predicted value, the following measures are calculated as mean of all BDTs and
+% stored in the bdt_figures struct Nr. 1:
+%  mean_PV = positive/negative predictive value
+%  mean_FDR = false discovery rate
+%  mean_recall = recall
 
-function [hae, naive] = plot_Conf(struct, folder, which_lig) % [hae, naive] = 
+
+function MPV = plot_Conf(struct, folder, which_lig) % [hae, naive] = 
 global bdt_figures
 
 if(~exist('struct','var') || isempty(struct))
@@ -38,7 +55,7 @@ if(folder~=0)
         mkdir(folder)
     end
 end
-%find right struct for single MM/Ligand tests
+%find right struct for single Ligand tests
 which_drug = 'CONTROL';
 
 if(~exist('which_lig','var') || isempty(which_lig) || (isinteger(which_lig) && which_lig==1))
@@ -78,7 +95,7 @@ for i=1:length(struct_tmp)
         save_name1= [save_name1 '_drug_' strrep(which_drug,'/',' and ') '_lig_' strrep(which_lig,'/',' and ') ];
 
     end
-    titles={'Significant shrinkage','No sign. proliferation','Significant proliferation'};
+    titles={'No sign. proliferation','Significant proliferation'};
 
     if(strfind(bdt_figures.(struct_tmp{i})(1).name,'RNA'))
         title_name = 'RNA, ';
@@ -88,156 +105,82 @@ for i=1:length(struct_tmp)
     if(~isempty(plot_id))
         title_name = [title_name 'drug ' strrep(which_drug,'_','\_') ', ligand ' which_lig];
     end
-    %if(~isfield(bdt_figures.(struct_tmp{i})(1).testing(plot_id),'TFP') || isnan(bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(1,1,2)))
+    %if(~isfield(bdt_figures.(struct_tmp{i})(1).testing(plot_id),'MPV') || isnan(bdt_figures.(struct_tmp{i})(1).testing(plot_id).MPV(1,1,2)))
         
         %Store confusion matrix, normed by either number of events in
         %respective column or row
-        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed = NaN([3,3,length(bdt_figures.(struct_tmp{i}))] );
-        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_normed = NaN([3,3,length(bdt_figures.(struct_tmp{i}))] );
+        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed = NaN([2,2,length(bdt_figures.(struct_tmp{i}))] );
+        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_normed = NaN([2,2,length(bdt_figures.(struct_tmp{i}))] );
         
         %Store raw Conf Matrix
-        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_Matrix = NaN([3,3,length(bdt_figures.(struct_tmp{i}))] );
+        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_Matrix = NaN([2,2,length(bdt_figures.(struct_tmp{i}))] );
         
-        %Store mean/std of normed Conf matrices
-        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_mean = NaN(3,3);
-        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_std = NaN(3,3);
-        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_mean = NaN(3,3);
-        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_std = NaN(3,3);
+        %Store mean/std over all trained BDTs of normed Conf matrices
+        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_mean = NaN(2,2);
+        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_std = NaN(2,2);
+        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_mean = NaN(2,2);
+        bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_std = NaN(2,2);
 
-        bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP = NaN(length(bdt_figures.(struct_tmp{i})),2,3);
+        bdt_figures.(struct_tmp{i})(1).testing(plot_id).MPV = NaN(length(bdt_figures.(struct_tmp{i})),1,2);
         length_conf = size(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed,2);
-    
         for j=1:length(bdt_figures.(struct_tmp{i}))
-            %getting ugly, restruct cMat by hand
+        %loop overall trained BDTs, and store Matrices normed by sum over
+        %respectrive row (col_normed) or column (row_normed)
             C_mat_tmp = bdt_figures.(struct_tmp{i})(j).testing(plot_id).C_mat;
             if(isempty(C_mat_tmp))
                 continue;
             end
             Max_Cs = max(length(bdt_figures.(struct_tmp{i})(j).testing(plot_id).Y_unique),length(bdt_figures.(struct_tmp{i})(j).testing(plot_id).bdt_class));
-            if(Max_Cs>1)
-                if(length(bdt_figures.(struct_tmp{i})(j).testing(plot_id).Y_unique)>length(bdt_figures.(struct_tmp{i})(j).testing(plot_id).bdt_class))
-                    missing_row = ismember(1:3,bdt_figures.(struct_tmp{i})(j).testing(plot_id).Y_unique+2);
-                else
-                    missing_row = ismember(1:3,bdt_figures.(struct_tmp{i})(j).testing(plot_id).bdt_class+2);
-                end
-
-                if(find(missing_row==0)==1)
-                    C_mat_tmp = [zeros(1,3); zeros(2,1) bdt_figures.(struct_tmp{i})(j).testing(plot_id).C_mat];
-                elseif(find(missing_row==0)==3)
-                    C_mat_tmp = [bdt_figures.(struct_tmp{i})(j).testing(plot_id).C_mat zeros(2,1); zeros(1,3)];
-                elseif(find(missing_row==0)==2)
-                    C_mat_tmp = [bdt_figures.(struct_tmp{i})(j).testing(plot_id).C_mat(1,1) 0 bdt_figures.(struct_tmp{i})(j).testing(plot_id).C_mat(1,2); ...
-                        zeros(1,3); bdt_figures.(struct_tmp{i})(j).testing(plot_id).C_mat(2,1) 0 bdt_figures.(struct_tmp{i})(j).testing(plot_id).C_mat(2,2)];
-                end
+            if(Max_Cs>1)                
                 bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(:,:,j) = C_mat_tmp./(repmat(nansum(C_mat_tmp,1),length_conf,1));
                 bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_normed(:,:,j) = C_mat_tmp./(repmat(nansum(C_mat_tmp,2),1,length_conf));
                 
                 bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_Matrix(:,:,j) = C_mat_tmp;
             elseif(Max_Cs==1)
-                col_id = ismember([-1 0 1],bdt_figures.(struct_tmp{i})(j).testing(plot_id).Y_unique);
-                bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(col_id,:,j) = 0;
-                bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(:,col_id,j) = 0;
-                bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(col_id,col_id,j) = 1;
-                bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_normed(col_id,:,j) = 0;
-                bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_normed(:,col_id,j) = 0;
-                bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_normed(col_id,col_id,j) = 1;
+                bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(:) = NaN;
+                bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_normed(:) = NaN;
             else
                 continue;           
             end
 
         end
         for k=1:length_conf
+            %This loop is needed if more than 0/1 decisions have to be
+            %made. Here, means over all BDTs are calculated for the normed
+            %confusion matrices stored above
             is_negative = 1:length_conf;
             is_negative(is_negative==k)=[];
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,k) = squeeze(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(k,k,:));
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,2,k) = squeeze(nansum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(is_negative,k,:),1));
+            bdt_figures.(struct_tmp{i})(1).testing(plot_id).MPV(:,1,k) = squeeze(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(k,k,:));
             for j=1:length_conf
                 bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_mean(j,k) = nanmean(squeeze(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(j,k,:)));
                 bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_std(j,k) = nanstd(squeeze(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_normed(j,k,:)));
                 bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_mean(j,k) = nanmean(squeeze(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_normed(j,k,:)));
                 bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_std(j,k) = nanstd(squeeze(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_normed(j,k,:)));
             end
-
-            mean_TP = bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_mean(k,k);
-            std_TP = bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_std(k,k);
-            mean_FP = nansum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_mean(is_negative,k,:),1);
-            std_FP = nansum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_std(is_negative,k,:).^2,1);
-            mean_FN = nansum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_mean(k,is_negative,:),1);
-            std_FN = nansum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_std(k,is_negative,:).^2,1);
+            %Some statistical properties are calculated (see description in
+            %the header)
+            mean_PV = bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_mean(k,k);
+            std_PV = bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_std(k,k);
+            mean_FDR = nansum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_mean(is_negative,k,:),1);
+            std_FDR = nansum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_col_std(is_negative,k,:).^2,1);
             mean_recall = bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_mean(k,k);
             std_recall = bdt_figures.(struct_tmp{i})(1).testing(plot_id).conf_row_std(k,k);
 
-            ratio = mean_TP/(mean_FP);
-            std_ratio = ratio * sqrt(std_TP^2/mean_TP^2 + std_FP/mean_FP^2);
+            ratio = mean_PV/(mean_FDR);
+            std_ratio = ratio * sqrt(std_PV^2/mean_PV^2 + std_FDR/mean_FDR^2);
             sprintf('Ratio of True/False for %s has mean %f +- %f',titles{k},ratio,std_ratio)
-            sprintf('F-measure is: %f +- ',2*mean_TP*mean_recall/(mean_TP+mean_recall))
+            sprintf('F-measure is: %f +- ',2*mean_PV*mean_recall/(mean_PV+mean_recall))
 
-            sprintf('True predictions for it have mean %f +- %f',mean_TP,std_TP)
-            sprintf('False predictions for it have mean %f +- %f',mean_FP,sqrt(std_FP))
-
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).mean_TP = mean_TP;
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).std_TP = std_TP;
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).mean_FP = mean_FP;
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).std_FP = std_FP;
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).mean_FN = mean_FN;
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).std_FN = std_FN;
+            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).mean_TP = mean_PV;
+            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).std_TP = std_PV;
+            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).mean_FP = mean_FDR;
+            bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).std_FP = std_FDR;
             bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).ratio = ratio;
             bdt_figures.(struct_tmp{i})(1).testing(plot_id).Conf(k).std_ratio = std_ratio;
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).growth_true = bdt_figures.(struct_tmp{i})(1).testing(plot_id).perc_1;
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).growth_false = sum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).perc_0 + bdt_figures.(struct_tmp{i})(1).testing(plot_id).perc_neg1);
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).neutral_true = bdt_figures.(struct_tmp{i})(1).testing(plot_id).perc_0;
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).neutral_false = sum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).perc_1 + bdt_figures.(struct_tmp{i})(1).testing(plot_id).perc_neg1);
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).neg_true = bdt_figures.(struct_tmp{i})(1).testing(plot_id).perc_neg1;
-            bdt_figures.(struct_tmp{i})(1).testing(plot_id).neg_false = sum(bdt_figures.(struct_tmp{i})(1).testing(plot_id).perc_0 + bdt_figures.(struct_tmp{i})(1).testing(plot_id).perc_1);
-
-    %         [~, p_val] = ttest(squeeze(bdt_figures.(struct_tmp{i})(1).conf_col_normed(k,k,:)),squeeze(sum(bdt_figures.(struct_tmp{i})(1).conf_col_normed(is_negative,k,:),1)));
-    %         sprintf('The t-test for %s has p-value of %f',titles{k},p_val)
         end
-    %end
-    hae =bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,3);
-    naive = bdt_figures.(struct_tmp{i})(1).testing(plot_id).growth_true;
-    
-    sprintf('Averaged true predictions have mean %f +- %f',nanmean((bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,3) + bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,2))./2),nanstd((bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,3) + bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,2))./2))
-    hae = nanmean((bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,3) + bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,2))./2);
-    boxplot((bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,3) + bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP(:,1,2))./2,'labels',{'True predictions'})
-    hold on
-%     error_shrinkage = sqrt(bdt_figures.(struct_tmp{i})(1).testing(plot_id).neg_true*bdt_figures.(struct_tmp{i})(1).testing(plot_id).neg_false/bdt_figures.(struct_tmp{i})(1).testing(plot_id).events);
-    error_neutral = sqrt(bdt_figures.(struct_tmp{i})(1).testing(plot_id).neutral_true*bdt_figures.(struct_tmp{i})(1).testing(plot_id).neutral_false/bdt_figures.(struct_tmp{i})(1).testing(plot_id).events); 
-    error_growth = sqrt(bdt_figures.(struct_tmp{i})(1).testing(plot_id).growth_true*bdt_figures.(struct_tmp{i})(1).testing(plot_id).growth_false/bdt_figures.(struct_tmp{i})(1).testing(plot_id).events);    
-    errorbar(1,.5,sqrt(error_growth^2+error_neutral^2),sqrt(error_growth^2+error_neutral^2),'Marker','.','Markersize',20,'Color','r')
-    
-%     %plot True/False assignment separately
-%     boxplot(reshape(bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP,size(bdt_figures.(struct_tmp{i})(1).testing(plot_id).TFP,1),[],1),'labels',repmat({'True pred','False pred'},1,length_conf));    
-%     hold on
-%     Y_ax = get(gca,'YLim');
-%     line([2.5 2.5],Y_ax)
-%     line([4.5 4.5],Y_ax)
-%     for j=1:length(titles)
-%         text(1+(j-1)*2,Y_ax(1)+0.1,titles{j});
-%     end
-%     
-%     errorbar(1,bdt_figures.(struct_tmp{i})(1).testing(plot_id).neg_true,error_shrinkage,error_shrinkage,'Marker','.','Markersize',20,'Color','k')
-%     errorbar(2,bdt_figures.(struct_tmp{i})(1).testing(plot_id).neg_false,error_shrinkage,error_shrinkage,'Marker','.','Markersize',20,'Color','r')
-%      
-%     errorbar(3,bdt_figures.(struct_tmp{i})(1).testing(plot_id).neutral_true,error_neutral,error_neutral,'Marker','.','Markersize',20,'Color','k')
-%     errorbar(4,bdt_figures.(struct_tmp{i})(1).testing(plot_id).neutral_false,error_neutral,error_neutral,'Marker','.','Markersize',20,'Color','r')
-%     
-% %     errorbar(5,bdt_figures.(struct_tmp{i})(1).testing(plot_id).growth_true,error_growth,error_growth,'Marker','.','Markersize',20,'Color','k')
-% %     errorbar(6,bdt_figures.(struct_tmp{i})(1).testing(plot_id).growth_false,error_growth,error_growth,'Marker','.','Markersize',20,'Color','r')
-%     errorbar(5,.5,error_growth,error_growth,'Marker','.','Markersize',20,'Color','k')
-%     errorbar(6,.5,error_growth,error_growth,'Marker','.','Markersize',20,'Color','r')
+    %Function returns MPV = (PPV+NPV)/2 and prints it
+    MPV = nanmean((bdt_figures.(struct_tmp{i})(1).testing(plot_id).MPV(:,1,1) + bdt_figures.(struct_tmp{i})(1).testing(plot_id).MPV(:,1,2))./2);    
+    sprintf('Averaged true predictions have mean %f +- %f',nanmean((bdt_figures.(struct_tmp{i})(1).testing(plot_id).MPV(:,1,1) + bdt_figures.(struct_tmp{i})(1).testing(plot_id).MPV(:,1,2))./2),nanstd((bdt_figures.(struct_tmp{i})(1).testing(plot_id).MPV(:,1,1) + bdt_figures.(struct_tmp{i})(1).testing(plot_id).MPV(:,1,2))./2))
 
-    h2 = get(gca,'Children');
-%     legend([h2(1) h2(2)],'Binomial relative true data','Binomial relative false data')
-%     ylabel('Relative true/false predictions','Interpreter','Tex');
-    legend([h2(1)],'Binomial relative true data')
-    ylabel('Relative true predictions','Interpreter','Tex');
-    
-    set(gcf,'Color','w');
-    title(sprintf('Statistics of training for %s',title_name));
-    if(folder~=0)
-        saveas(gcf,save_name1,'fig')
-        saveas(gcf,save_name1,'pdf')
-    end
 end
 
