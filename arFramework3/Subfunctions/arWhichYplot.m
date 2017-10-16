d8d8fa888f992596ed1184164443e4c89db053d3
Helge Hass <helge.hass@fdm.uni-freiburg.de>
2017-10-16 11:40:26 +0200

    error model fixes in plotting/ple

--- arFramework3/Subfunctions/arWhichYplot.m
+++ arFramework3/Subfunctions/arWhichYplot.m
@@ -69,7 +69,7 @@ else
     elseif ar.config.ploterrors == 0  % don't externally control plotting. Instead evaluate ar.config.fiterrors and availability of exp. errors.
         if isempty(ar.model(m).data(d).yExpStd)
             plotopt = 5;
-        elseif sum(isnan(ar.model(m).data(d).yExpStd(it,iy)) )==0  && ar.config.fiterrors~=1  % all exp. errors available && exp. errors used
+        elseif sum(isnan(ar.model(m).data(d).yExpStd(it,iy)) & ~isnan(ar.model(m).data(d).yExp(it,iy)))==0  && ar.config.fiterrors~=1  % all exp. errors available && exp. errors used
             plotopt = 2;
         elseif (ar.config.fiterrors==1 && ar.config.ploterrors==0) || (sum(~isnan(ar.model(m).data(d).yExpStd(it,iy)) & ~isnan(ar.model(m).data(d).yExp(it,iy)))==0   && ar.config.fiterrors~=-1) % no exp. errors available && error model used
             plotopt = 3;
