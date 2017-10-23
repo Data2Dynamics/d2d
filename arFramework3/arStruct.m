d27e8880b297bba8c10f0643592d536048c08bcb
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2017-10-23 17:27:12 +0200

    Extended arStruct.m a little

--- arFramework3/arStruct.m
+++ arFramework3/arStruct.m
@@ -3,11 +3,20 @@
 %
 %   Model - Model
 %     - res                 Residual
-%     - sres                Sensitivity of the residual
+%     - sres                Sensitivity of the residual (here the observables and parameters are specified in whatever space they are fitted (log or not))
+%     - constr              Constraint based residuals
 %     - chi2                Sum of squared residuals (when fitting errors, this is *not* the objective function)
 %                             Given by sum( ar.model.data.res^2 )
-%     - chi2fit             Sum of squared residuals and error residuals (this is typically the objective function)
-%                             Given by sum( ar.model.data.res.^2 + ar.model.data.reserr.^2 - ar.config.add_c )
+%     - chi2err             Sum of error residuals
+%                             Given by sum(ar.model.data.reserr.^2) + ar.config.add_c * sum(ar.model.data.reserr>0)
+%                             These come from the normalization constant of the likelihood which needs to be taken into account when estimating sigma.
+%                             Note that the add_c term comes from the fact that we need to provide a residual to lsqnonlin. Since the normalization term is proportional to
+%                             sqrt(ln(sigma)+add_c), the add_c is used to ensure positivy under the sqrt. It is later subtracted again when computing the objective function.
+%     - chi2constr          Sum of squared constraint residuals.
+%                             Given by sum(ar.constr.^2)
+%     - chi2fit             Sum of squared residuals and error residuals (this is typically the objective function when there are no constraints).
+%                             Given by ar.chi2 + ar.chi2err
+%                             Hint: the log likelihood can be computed as 2*ar.ndata*log(sqrt(2*pi)) + ar.chi2fit;
 %     - (chi2s)             Present after running an arFits or LHS. List of objective functions with NaN's for ones that did not complete.
 %     - (ps)                Present after running an arFits or LHS. List of parameter vectors with NaN's for ones that did not complete
 %     - pLabel              Parameter names
@@ -20,7 +29,7 @@
 %     - p                   Parameter values (note that depending on qLog10 they are in logspace or not)
 %     - type                Type of prior (0=box, 1=normal, 2=uniform with normal bounds, 3=L1 prior)
 %
-%     - condition           Each experimental condition
+%     - condition           Experimental conditions
 %         dLink             Index to which data this condition belongs
 %         tExp              Time points used in fitting
 %         tFine             Time points for the fine simulation
@@ -40,14 +49,14 @@
 %                           condition?
 %         sxExpSimu         Model state sensitivities for fitting [nExp,nState,nPars]
 %                               These sensitivities are always specified in
-%                               *linear parameter space*.
+%                               linear parameter space.
 %         sxFineSimu        Fine model state sensitivities  [nFine,nStates,nPars]
 %                               When parameters are being fitted in
 %                               logspace, these sentivities are specified
-%                               w.r.t. *logspace*.
+%                               with respect to logspace.
 %                               Note: dx/dlog10(p) = dx/dp * ln(10) * 10^log10(p)
 %
-%     - ss_condition        Steady state condition used for equilibration
+%     - ss_condition        Steady state conditions used for equilibration
 %                           (mostly analogous to condition)
 %         src               Which condition to use as reference for the
 %                           steady state equilibration.
@@ -61,7 +70,7 @@
 %         tExp              Time points used in fitting
 %         tFine             Time points for the fine simulation
 %         yExpSimu          Observable trajectories for this data
-%         yFineSimu         Observable trajectories for this data
+%         yFineSimu         Observable fine grained trajectories
 %         res               Residual vector corresponding to the data
 %         sres              Sensitivities of the residual vector
 %         reserr            Part of the residual vector that corresponds to
