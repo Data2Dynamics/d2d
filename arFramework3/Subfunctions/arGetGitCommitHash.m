ec407a5f9a36fe300394c27b7c72b5575c13bf20
rengesser <17144266+rengesser@users.noreply.github.com>
2018-03-07 17:45:45 +0100

    save hash of the git commit in ar.info for better reproducibility of older projects

---
+++ arFramework3/Subfunctions/arGetGitCommitHash.m
@@ -0,0 +1,27 @@
+% Get the hash of the git commit for saving in ar.info.gitCommitHash
+%
+% The D2d version used to build the ar struct can be obtained by
+% https://github.com/Data2Dynamics/d2d/commit/hash
+% where 'hash' has to be replaced by the string saved in ar.info.gitCommitHash
+%
+% commit_hash = arGetGitVersion(repo_path)
+
+function commit_hash = arGetGitCommitHash(repo_path)
+
+if(~exist('repo_path','var'))
+    repo_path = fileparts(which('arInit.m'));
+end
+
+old_dir = pwd;
+cd(repo_path)
+try
+    if(isunix)
+        [~, commit_hash] = system('git rev-parse HEAD 2>/dev/null');
+    else
+        [~, commit_hash] = system('git rev-parse HEAD 2>nul');
+    end
+catch
+    commit_hash = '';
+end
+
+cd(old_dir)
