aa15ecd148f727b9e39b77b75aa9f102f9c452da
Carolin Loos <carolin.loos@helmholtz-muenchen.de>
2018-05-15 13:22:26 +0200

    setup file optimizer changed

--- arFramework3/Examples/Crauste_ImmuneCells_CellSystems2017/Setup.m
+++ arFramework3/Examples/Crauste_ImmuneCells_CellSystems2017/Setup.m
@@ -10,20 +10,30 @@ arCompileAll;
 
 ar.config.fiterrors = -1;
 
-ar.p(5:end) = log10([ ...
-    0.587811356745978;%delta_EL
-    0.0249390061548269 % delta_LM
-    0.00941887996782282; % delta_NEit 
-    2.15367872500831e-05; % mu_EE
-    3.57810426702153e-08 % mu_LE
-    7.49479597712756e-06; % mu_LL
-    0.746532818498732; % mu_N
-    0.0551056351722066; % mu_P
-    1.79033591694338e-07; % mu_PE
-    1.82649004874779e-05; % mu_PL
-    0.642885381731727; % % rho_E
-    0.150783084474492 % rho_P
-    ]);
+ar.p(5:end) = log10([ ...   
+    0.59; % delta_EL 
+    0.025; % delta_LM
+    0.009; % delta_NE 
+    21.5e-6; % mu_EE 
+    3.6e-8; % mu_LE 
+    7.5e-6; % mu_LL
+    0.75; % mu_N 
+    5.5e-2; % mu_P 
+    1.8e-7; % mu_PE 
+    1.8e-5; % mu_PL 
+    0.64; % rho_E 
+    0.15; % rho_P
+    ]); 
 
-ar.lb(:) = -10;
-arPlot
+ar.lb([9 10 13]) = -10;
+
+%Set fmincon interior-point optimizer
+ar.config.maxsteps = 1.e4;
+ar.config.optimizer = 2;
+ar.config.atol = 1.e-10;
+ar.config.rtol = 1.e-10;
+ar.config.optim.MaxIter = 1.e4;
+
+arFit
+arPlot 
+
