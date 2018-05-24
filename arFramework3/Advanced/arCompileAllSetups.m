6450fb0f1a6736d5278007f5edb8ec99ddc61973
clemenskreutz <ckreutz@fdm.uni-freiburg.de>
2018-05-24 13:36:25 +0200

    The function is running, now.

--- arFramework3/Advanced/arCompileAllSetups.m
+++ arFramework3/Advanced/arCompileAllSetups.m
@@ -26,19 +26,25 @@ for i=1:length(all_files)
     bol(i) = ~isempty(regexp(name,'^[sS]etup*')) && strcmp(ext,'.m')==1;
 end
 
+fidlog = fopen('arCompileAllSetups.log','w');
+
 setup_files = all_files(bol);
-% cdisp(setup_files)
-
-
-try
-   delete(gcp('nocreate'))
+fprintf('The following setup files were found and will be subsequently used for compiling: \n\n');
+fprintf(fidlog,'The following setup files were found and will be subsequently used for compiling: \n\n');
+for i=1:length(setup_files)
+    fprintf('%s\n',setup_files{i});
+    fprintf(fidlog,'%s\n',setup_files{i});
 end
-try
-   parpool('local')
+fprintf('\n\n');
+fprintf(fidlog,'\n\n');
+
+% create parallel pool if not yet existing:
+p = gcp('nocreate');
+if isempty(p)
+    parpool('local')
 end
 
 pfad = pwd;
-fidlog = fopen('arCompileAllSetups.log','w');
 
 for i=1:length(setup_files)
     fprintf(fidlog,'%s will be executed ...\n\n' ,setup_files{i});
@@ -51,31 +57,36 @@ for i=1:length(setup_files)
             [str, fid] = arTextScan(fid, '%s\n', 1, 'CommentStyle', '%');
             
             if ~isempty(str) && iscell(str)                
-                str = strtrim(str{1});
-                if ~isempty(str) && ischar(str{1})
-                    if sum(~cellfun(@isempty,regexp(str,'arInit')))>0
-                        fprintf(fidlog,'%s\n',str{1});
-                        %         eval(str);
-                    elseif sum(~cellfun(@isempty,regexp(str,'arLoadModel\(')))>0
-                        fprintf(fidlog,'%s\n',str{1});
-                        %         eval(str);
-                    elseif sum(~cellfun(@isempty,regexp(str,'arLoadData\(')))>0
-                        fprintf(fidlog,'%s\n',str{1});
-                        %         eval(str);
-                    elseif sum(~cellfun(@isempty,regexp(str,'arCompileAll')))>0
-                        fprintf(fidlog,'%s\n',str{1});
-                        %         eval(str);
+                if iscell(str)
+                    str = strtrim(str{1});
+                    if iscell(str)
+                        str = strtrim(str{1});
+                    end
+                end
+                if ~isempty(str) && ischar(str)
+                    if ~isempty(regexp(str,'arInit'))
+                        fprintf(fidlog,'%s\n',str);
+                                eval(str);
+                    elseif ~isempty(regexp(str,'arLoadModel\('))
+                        fprintf(fidlog,'%s\n',str);
+                                eval(str);
+                    elseif ~isempty(regexp(str,'arLoadData\('))
+                        fprintf(fidlog,'%s\n',str);
+                                eval(str);
+                    elseif ~isempty(regexp(str,'arCompileAll'))
+                        fprintf(fidlog,'%s\n',str);
+                                eval(str);
                     else
                         
                     end
                 else
-%                     fprintf('%s\n',str{1});
+                    fprintf('Empty string str{1}= %s\n',str{1});
                 end
             end
         end
         fprintf(fidlog,'\n\n');
         fclose(fid);
-    catch
+    catch ERR
         warning('\n%s failed !!!!!!!!! \n\n' ,setup_files{i});
         disp(lasterror);
         fprintf(fidlog,'\n%s failed !!!!!!!!! \n\n' ,setup_files{i});
