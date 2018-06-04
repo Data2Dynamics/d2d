b84e655741ec6cec9e23a88bdfc57e11c600a3bd
Helge Hass <helge.hass@fdm.uni-freiburg.de>
2018-06-04 11:21:43 +0200

    updates of bestFit for Beer and Crauste

--- arFramework3/Examples/Crauste_ImmuneCells_CellSystems2017/Setup.m
+++ arFramework3/Examples/Crauste_ImmuneCells_CellSystems2017/Setup.m
@@ -10,11 +10,12 @@ arCompileAll;
 ar.config.fiterrors = -1;
 
 %Set fmincon interior-point optimizer
-ar.config.maxsteps = 1.e4;
-ar.config.optimizer = 2;
+ar.config.maxsteps = 4.e4;
+ar.config.optimizer = 1;
 ar.config.atol = 1.e-10;
 ar.config.rtol = 1.e-10;
 ar.config.optim.MaxIter = 1.e4;
+ar.config.optim.InitTrustRegionRadius = 1;
 
 arLoadPars('bestFit')
 arPrint
