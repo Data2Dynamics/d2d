db4e312340c9fb3c432192fadf52f3a154c0a5b1
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2018-08-07 17:47:09 +0200

    Reverse logic on useSparseJac

--- arFramework3/arCompileAll.m
+++ arFramework3/arCompileAll.m
@@ -758,11 +758,11 @@ if length(ar.model(m).sym.x)<100 && length(ar.model(m).p)<500
     tmpzeros = (ar.model(m).N .* ar.model(m).sym.C) * ar.model(m).sym.dfvdx;
     ar.model(m).nnz = arnansum(arnansum(logical(tmpzeros~=0))) + arnansum(arnansum(logical(tmpzeros~=0))==0);
     if(length(ar.model(m).x) * log(length(ar.model(m).x)) > ar.model(m).nnz)
-        ar.config.useSparseJac = 1;
+    elseif ( ar.config.useSparseJac == 1 )
+        ar.config.useSparseJac = 0;
     end
 else
     arFprintf(2, 'Model m%i too large (%i variables, %i parameters)... skipping check of model structure\n', m, length(ar.model(m).sym.x), length(ar.model(m).p));
-    ar.config.useSparseJac = 1;
     tmpzeros = (ar.model(m).N .* ar.model(m).sym.C) * ar.model(m).sym.dfvdx;
     ar.model(m).nnz = arnansum(arnansum(logical(tmpzeros~=0))) + arnansum(arnansum(logical(tmpzeros~=0))==0);
 end
