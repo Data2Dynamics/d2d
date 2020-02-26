eab6b59074e583d489a9dce29ceeafcea5102af6
JanineEgert <egert@imbi.uni-freiburg.de>
2020-02-26 10:30:59 +0100

    *.xml and *.tsv can be specified as input argument

--- arFramework3/ImportExport/arImportPEtab.m
+++ arFramework3/ImportExport/arImportPEtab.m
@@ -1,9 +1,11 @@
-% arImportPEtab(filename, doPreEquilibration, tstart)
+% arImportPEtab(name, doPreEquilibration, tstart)
 %
 % Import PEtab model and data format to Data2Dynamics. 
 %
-%       name                String that must be contained in the filenames
-%                           of all files to load
+%       name                Cell array of input functions []
+%                           {'namemodel1.xml','_OBS_model1.tsv','_COND_model1.tsv','_MEAS_model1.tsv','_PARS_model.tsv'}
+%                           if name is empty, pattern search of *.xml
+%                           *obs*.tsv *cond*.tsv *meas*.tsv *pars*.tsv is performed
 %       doPreEquilibration  Apply pre-equilibration if specified in PEtab files [true]
 %       tstart              Starting time for pre-equilibration [0]
 %
@@ -12,6 +14,11 @@
 %
 % References
 %   - https://github.com/ICB-DCM/PEtab/blob/master/doc/documentation_data_format.md
+%
+% Examples:
+% arImportPEtab
+% arImportPEtab({'namemodel1_l2v4.xml','_OBS_model1.tsv'})
+% arImportPEtab({'namemodel1_l2v4.xml',[],'_PARS_model.tsv'})
 
 function arImportPEtab(name, doPreEquilibration)
 global ar
@@ -19,23 +26,26 @@ global ar
 if(isempty(ar))
     error('please initialize by arInit')
 end
-
 if isfield(ar, 'model')
     error('Existing ar workspace detected. Please initialize by arInit for PEtab import')
 end
-
-if ~exist('name','var') || isempty(name)
-    name = '';
-else
-    name = [name '*'];
-end
-
-if ~exist('doPreEquilibration') || isempty(doPreEquilibration)
+if ~exist('doPreEquilibration','var') || isempty(doPreEquilibration)
     doPreEquilibration = true;
 end
+if exist('name','var') && ~iscell(name)
+    name = {name};
+end
+    
 
 %TODO: read in multiple sbmls & save these paths in ar struct
-sbmlmodel = dir(['**' filesep '*' name '.xml']);
+if ~exist('name','var') || isempty(name) 
+    sbmlmodel = dir(['**' filesep '*.xml']);
+else
+    sbmlmodel = dir(['**' filesep name{1}]);
+    if isempty(sbmlmodel)
+        sbmlmodel = dir(['**' filesep name{1} '.xml']);
+    end
+end
 if isempty(sbmlmodel)
     error('No sbml file found! Switch your path to working directory.');
 end
@@ -54,10 +64,27 @@ arParseSBML([sbmlmodel.folder filesep sbmlmodel.name])
 arLoadModel(strrep(sbmlmodel.name,'.xml',''))
 
 % make dir case sensitive!
-PEobs = dir([pe_dir filesep sprintf('%s%s*.tsv', name, 'obs')]);
-PEmeas = dir([pe_dir filesep sprintf('%s%s*.tsv', name, 'meas')]);
-PEconds = dir([pe_dir filesep sprintf('%s%s*.tsv', name, 'cond')]);
-PEparas = dir([pe_dir filesep sprintf('%s%s*.tsv', name, 'par')]);
+
+if ~exist('name','var') || isempty(name) || length(name)<2 || isempty(name{2})
+    PEobs = dir(name{2});     
+else
+    PEobs = dir([pe_dir filesep sprintf('*%s*.tsv', 'obs')]);
+end
+if ~exist('name','var') || isempty(name) || length(name)<3 || isempty(name{3})
+    PEmeas = dir(name{3});
+else
+    PEmeas = dir([pe_dir filesep sprintf('*%s*.tsv', 'meas')]);
+end
+if ~exist('name','var') || isempty(name) || length(name)<4 || isempty(name{4})
+    PEconds = dir(name{4});
+else
+    PEconds = dir([pe_dir filesep sprintf('*%s*.tsv', 'cond')]);
+end
+if ~exist('name','var') || isempty(name) || length(name)<5 || isempty(name{5})
+    PEparas = dir(name{5});
+else
+    PEparas = dir([pe_dir filesep sprintf('*%s*.tsv', 'par')]);
+end
 
 if length(PEparas) > 1 || length(PEmeas) > 1 || length(PEconds) > 1 || length(PEobs) > 1
     error('Found more than one peTAB file.')
@@ -69,8 +96,7 @@ for m = 1:length(ar.model)
     [T{m,1}, T{m,2}] = ...
         arLoadDataPEtab([pe_dir filesep PEmeas.name],[pe_dir filesep PEobs.name],m);
 end
-Tcond = arLoadCondPEtab([pe_dir filesep PEconds.name]);
-
+arLoadCondPEtab([pe_dir filesep PEconds.name]);
 
 % Compilation
 arCompileAll
