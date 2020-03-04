e598812061721615f4f42e5b68106692622cb32b
fgwieland <franz-georg.wieland@fdm.uni-freiburg.de>
2020-03-04 08:37:37 +0100

    Create arTestPEtabCompatibility

---
+++ arFramework3/ImportExport/arTestPEtabCompatibility.m
@@ -0,0 +1,24 @@
+function arTestPEtabCompatibility()
+% arTestPEtabCompatibility()
+%
+% Test PEtab compatibility of current PEtab folder by 
+%     1 Importing PEtab model
+%     2 Exporting D2D model to PEtab
+%     3 Importing Exported model again
+%     4 Comparing two d2d models after initial import and export-import cycle
+arInit
+arImportPEtab
+arSimu(0,1,0)
+arSave('FirstImport')
+FirstImportStruct = arDeepCopy(ar);
+arExportPEtab('d2dExport')
+clearvars -except ImportStruct
+arInit
+arImportPEtab('d2dExport')
+arSimu(0,1,0)
+arSave('SecondImport')
+SecondImportStruct = arDeepCopy(ar);
+arCompare(FirstImportStruct,SecondImportStruct,'main')
+
+end
+
