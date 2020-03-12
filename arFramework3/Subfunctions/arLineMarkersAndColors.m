87380e4b327c493a7a9635656d0192f282c6bf4b
Adrian Hauber <adrian.hauber@fdm.uni-freiburg.de>
2020-03-12 15:07:00 +0100

    new colors
    new color sets added (matlab default colors and dMod colors). Color set can be chosen by setting ar.config.plotColorSet. Old colors are accessed via setting ar.config.plotColorSet = 'd2d_legacy'

--- arFramework3/Subfunctions/arLineMarkersAndColors.m
+++ arFramework3/Subfunctions/arLineMarkersAndColors.m
@@ -2,6 +2,7 @@
 % create line styles and color
 
 function C = arLineMarkersAndColors(j,n,color,markerstyle,linestyle)
+global ar
 
 % test function
 if(nargin==0 || isempty(j))
@@ -27,6 +28,12 @@ if(n>Nmax)
     n=Nmax;
 end
 
+if ~isfield(ar.config, 'plotColorSet')
+    ar.config.plotColorSet = 'matlab_default';
+end
+
+% Legacy colors
+if strcmp(ar.config.plotColorSet, 'd2d_legacy')
 if(~exist('color','var') || isempty(color))
     if(n==1)
         colors = [0 0 0];
@@ -51,6 +58,56 @@ if(~exist('color','var') || isempty(color))
     end
     jc = mymod(j,length(colors));
     color = colors(jc,:);
+end
+
+% dMod colors
+elseif strcmp(ar.config.plotColorSet, 'dMod')
+if(~exist('color','var') || isempty(color))
+    if n < 11
+        colorvec = [0 0 0; 
+            197 0 11; 
+            0 132 209; 
+            87,157,28; 
+            255,149,14; 
+            75,31,111; 
+            204,121,167; 
+            0,100,0; 
+            240,228,66; 
+            139,69,19]/255;
+        colors = colorvec(1:n,:);
+    else
+        colors = jet(n-1);
+        colors = bsxfun(@rdivide, colors, sqrt(sum(colors.^2,2)));
+        colors = [0 0 0; colors];
+    end
+    jc = mymod(j,length(colors));
+    color = colors(jc,:);
+end
+
+% MATLAB default colors
+elseif strcmp(ar.config.plotColorSet, 'matlab_default')
+if(~exist('color','var') || isempty(color))
+    if(n==1)
+        colors = [0 0 0];
+    elseif n < 7
+        colorvec = [0, 0.4470, 0.7410;
+            0.8500, 0.3250, 0.0980;
+            0.9290, 0.6940, 0.1250;
+            0.4940, 0.1840, 0.5560;
+            0.4660, 0.6740, 0.1880;
+            0.3010, 0.7450, 0.9330;
+            0.6350, 0.0780, 0.1840];
+            colors = colorvec(1:n,:);
+    else
+    colors = jet(n-1);
+    colors = bsxfun(@rdivide, colors, sqrt(sum(colors.^2,2)));
+    colors = [0 0 0; colors];
+    end
+    jc = mymod(j,length(colors));
+    color = colors(jc,:);
+end
+else
+    error('Invalid value in ar.config.plotColorSet. Use ''matlab_default'', ''dMod'' or ''d2d_legacy''.')
 end
 
 if(~exist('markerstyle','var') || isempty(markerstyle))
