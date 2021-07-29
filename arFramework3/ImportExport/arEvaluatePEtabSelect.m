3a1e6ad0781e1915f80e9b341e4408d5048e21ab
Adrian Hauber <adrian.hauber@fdm.uni-freiburg.de>
2021-07-29 11:52:43 +0200

    rough sketch for arEvaluatePEtabSelect

---
+++ arFramework3/ImportExport/arEvaluatePEtabSelect.m
@@ -0,0 +1,19 @@
+function modelReporFile = arEvaluatePEtabSelect(modelSpecFile, constrFile, config)
+
+% 
+%
+
+% arReadYaml(modelSpecFile)
+% arReadYaml(constrFile)
+% 
+% for i = 1:modelSpecs
+%   arInit
+%   arRead... (includes arImportPEtab)
+%   do optimization with config
+%   check constraints
+%   arWrite...
+% end
+% 
+% update status
+
+end
