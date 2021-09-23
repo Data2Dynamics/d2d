ece813a9f8594f275046890d50e2318cb9e5c2ce
fgwieland <franz-georg.wieland@fdm.uni-freiburg.de>
2021-09-23 11:32:16 +0200

    Petab-Select functionality first version of file

---
+++ arFramework3/Advanced/arPetabSelect.m
@@ -0,0 +1,37 @@
+%% Work in Progress
+
+function arPetabSelect()
+%% Parse function input
+
+
+%% Write selection problem yaml with input
+
+
+%% Check if petab_select is installed in system commands and give error if not yet installed
+
+
+%% Run petab_select code with system commands
+
+command = [
+sprintf(append('petab_select candidates \\ \n ',  ...
+'-y model_selection\', filesep, 'selection_problem.yaml \\ \n ', ...
+'-s output\', filesep, 'state.dill \\  \n ',...
+'-o output\', filesep, 'models.yaml  \\ \n ', ...
+'-m brute_force \\ \n ', ...
+'-l 3') )
+] ;
+[status,cmdout] = system(command)
+
+
+%% Import models.yaml written by petab_select script
+
+
+%% Run d2d fits & calculate criterion values with d2d functions
+
+%% Write calibrated first iteration .yaml including criterion values
+
+%% use petab_select candidates -b calibrated first iteration.yaml with method (forward selection...)
+
+%% Final output 
+
+end
