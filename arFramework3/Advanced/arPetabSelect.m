b410e567f8aa2887349c9fc942d0bb8feec2c5ee
Adrian Hauber <adrian.hauber@fdm.uni-freiburg.de>
2021-09-27 15:34:35 +0200

    closed the loop
    now does multiple iterations of PEtab-select until no more candidate models are proposed

--- arFramework3/Advanced/arPetabSelect.m
+++ arFramework3/Advanced/arPetabSelect.m
@@ -2,95 +2,104 @@
 % Optional extension in different function: Write selection problem yaml with input
 % Currently only one criterion supported
 
-function arPetabSelect(venvActPath)
+function arPetabSelect(venvActPath, selectionProblem, method, level, initialModel, CalibYamlOut, estimationRoutine, iterationCounter)
+if ~exist('iterationCounter') || isempty(iterationCounter)
+    iterationCounter = 1;
+end
 %% Parse function input
 
+% standard settings
+if ~exist('selectionProblem') || isempty(selectionProblem)
+    selectionProblem = 'selection_problem.yaml';
+end
+if ~exist('method') || isempty(method)
+    method = 'brute_force';
+end
+if ~exist('level') || isempty(level)
+    level = 3;
+end
+if ~exist('initialModel') || isempty(initialModel)
+    initialModel = '';
+end
+if ~exist('CalibYamlOut') || isempty(CalibYamlOut)
+    CalibYamlOut = 'calibrated_it_001.yaml';
+end
+if ~exist('estimationRoutine') || isempty(estimationRoutine)
+    estimationRoutine = @arFit;
+end
 
-
-
-
-%% Check if petab_select is installed in system commands and give error if not yet installed
 initstr = '';
 if exist('venvActPath') && ~isempty(venvActPath)
     initstr = sprintf('source %s; ', venvActPath);
 end
 
-command = [initstr, 'petab_select --help'];
-[status,~] = system(command);
+%% Check if petab_select installation
+syscom = [initstr, 'petab_select --help'];
+[status,~] = system(syscom);
 if status ~= 0
-    error(sprintf('petab_select could not be called as a system command.\nPlease make sure to install petab_select using pip install petab_select on your machine.'))
+    error(sprintf('Calling petab_select from the command line failed.\nPlease check your Python environment and the PEtab-select installation.'))
 end
 
+%% Call PEtab-select to generate candidate models
+fprintf('arPEtabSelect: Generating candidate models...\n')
+SelectionProblem = ReadYaml(selectionProblem);
 
-
-%% Run petab_select code with system commands
-SelectionProblem = ReadYaml('selection_problem.yaml');
-
-% Create output folder? Currently still missing
-if ~isfolder('output')
-    mkdir('output')
-end
-command = [initstr,...
+syscom = [initstr,...
 'petab_select candidates ',  ...
-' -y selection_problem.yaml ', ...
+' -y ', selectionProblem, ...
 ' -s output', filesep, 'state.dill',...
 ' -o output', filesep, 'models.yaml', ...
-' -m brute_force', ...
-' -l 3'
+' -m ', method, ...
+' -l ', num2str(level), ...
 ];
-[status,cmdout] = system(command);
+if ~isempty(initialModel)
+    syscom = [syscom, ' -b ' ,initialModel];
+end
+[status,cmdout] = system(syscom);
 
 if status ~= 0
     error(sprintf('Error while running petab_select from command line.\n Command line message:\n %s',cmdout)); %#ok<SPERR>
 end
 
-%% Import models.yaml written by petab_select script
+%% Process candidate models
+iterationCounter %debug
 CandidateModels = ReadYaml(['output' filesep 'models.yaml']);
-
-
-%% Run d2d fits & calculate criterion values with d2d functions
 nModels = size(CandidateModels,2);
-for jModel = 1:nModels
-    % collect PEtab files 
-    %bb = ReadYaml(CandidateModels{jModel}.petab_yaml);
-    %lastfilesepPos = find(CandidateModels(jModel).petab_yaml == filesep, 1, 'last');
-    %PeTsvPath = CandidateModels(jModel).petab_yaml(1:lastfilesepPos);
-    %PeTsvPath = cellfun(@(x) [PeTsvPath,x],{bb.sbml_files, bb.observable_files, bb.measurement_files, bb.condition_files, bb.parameter_file},'UniformOutput',false);
-    
+
+if nModels == 0
+    fprintf('arPEtabSelect: Finished after iteration %i - no candidate models found.\n', iterationCounter-1)
+    return
+end
+fprintf('arPEtabSelect: Calibrating candidate models...\n')
+
+for jModel = 1:nModels    
     arInit
     doPreEq = false;
-    
     arImportPEtab(CandidateModels{jModel}.petab_yaml,doPreEq)
-    % Fit model 
-    arFit
+    estimationRoutine;
     [~, allmerits] = arGetMerit;
-    
-    % Save criteria
+            
     % ADAPT FOR MULTIPLE CRITERIA
     switch SelectionProblem.criterion
-        case 'AICc'
-             bb(jModel).criteria = allmerits.loglik;  
+        case 'AIC'
+             theCriterion = allmerits.loglik;  
         case 'LogL'
-             bb(jModel).criteria = allmerits.loglik;  
+             theCriterion = allmerits.loglik;  
         otherwise
-             error('Invalid criteria for model nr%i:\n%s',jModel,CandidateModels(jModel).petab_yaml)
+             error('Invalid criteria for model nr%i:\n%s',jModel,CandidateModels{jModel}.petab_yaml)
     end
-    
-    
-    
 end
 
-%% Write calibrated first iteration .yaml including criterion values
+calibCands = CandidateModels;
+for jModel = 1:nModels
+    calibCands{jModel}.criteria.(SelectionProblem.criterion) = theCriterion;
+    calibCands{jModel}.estimated_parameters = 'null';
+    % add values of estimated parameters
+end 
+WriteYaml(CalibYamlOut,calibCands);
 
-
-
-
-%% use petab_select candidates -b calibrated first iteration.yaml with method (forward selection...)
-
-
-
-
-
-%% Final output 
-
+%% Next iteration
+fprintf('arPEtabSelect: Iteration %i complete. Continuing with next iteration\n',iterationCounter)
+nextIterationYamlOut = sprintf('calibrated_it_%03i.yaml',iterationCounter+1);
+arPetabSelect(venvActPath, selectionProblem, method, level, CalibYamlOut, nextIterationYamlOut, estimationRoutine,iterationCounter+1)
 end
