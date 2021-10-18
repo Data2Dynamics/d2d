a23257b483dd35946d2a793fee4bbd0adb7808b4
JanineEgert <egert@imbi.uni-freiburg.de>
2021-10-18 09:36:46 +0200

    arSetParsPattern with strncmp, if just patterns which start with pattern should be replaced

--- arFramework3/arSetParsPattern.m
+++ arFramework3/arSetParsPattern.m
@@ -1,8 +1,14 @@
 % arSetParsPattern(pattern, options)
 % 
 % pattern	pattern of the parameter name
+% {pattern, n} maximum number of characters to compare
 % options   works in the same manner as arSetPars. See there for more
 %           information
+%
+% Example:
+% arSetParsPattern('a_',5,1,1,-5,-2)
+% arSetParsPattern({'a_',2},5,1,1,-5,-2) 
+% 2nd option: just if pattern is in the beginning of the parameter name
 % 
 % see also arSetPars
 
@@ -10,6 +16,10 @@ function arSetParsPattern(varargin)
 
 global ar
 
-pLabels = ar.pLabel(~cellfun(@isempty, strfind(ar.pLabel, varargin{1})));
+if iscell(varargin{1})
+    pLabels = ar.pLabel(strncmp(ar.pLabel, varargin{1}{1},varargin{1}{2}));
+else
+    pLabels = ar.pLabel(~cellfun(@isempty, strfind(ar.pLabel, varargin{1})));
+end
 
 arSetPars(pLabels, varargin{2:end});
