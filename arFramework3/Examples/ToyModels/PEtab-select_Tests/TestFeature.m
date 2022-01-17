c1453378d888157920b0024b0de6c95f6ad86e4c
Adrian Hauber <adrian.hauber@fdm.uni-freiburg.de>
2022-01-17 14:36:02 +0100

    add PEtab-select test cases

---
+++ arFramework3/Examples/ToyModels/PEtab-select_Tests/TestFeature.m
@@ -0,0 +1,49 @@
+% do all
+cases = {'0001', '0002', '0003', '0004', '0005', '0006', '0007', '0008'};
+Ncases = numel(cases);
+
+isOk = NaN(Ncases,1);
+%parpool(4)
+for i = 1:Ncases
+    cd(cases{i})
+    system('rm -r output')
+    try
+    arPEtabSelect('~/Work/Projects/P106_D2D-PEtab-select/_python_venv/bin/activate')    
+    
+    expected = ReadYaml('expected.yaml');
+    actual = ReadYaml('petab-select/selected_model.yaml');
+    selProblem = ReadYaml('petab_select_problem.yaml');
+    isOk(i) = comparePeTabYamlStruct(expected, actual, selProblem.criterion);
+    if isOk(i) ~= 1
+        fprintf('test case %s failed\n', cases{i})
+    end
+    catch
+        fprintf('error during test of case %s\n', cases{i})
+    end
+    cd ..
+end
+
+if sum(isOk) == Ncases
+    fprintf('all tests passed\n')
+else
+    error('did NOT pass all tests')
+end
+
+function isOk = comparePeTabYamlStruct(expected, actual, criterion)
+% check criteria, model_subspace_id and values of estimated parameters
+
+isOk = 1;
+if round(actual.criteria.(criterion),3) ~= round(expected.criteria.(criterion), 3)
+    isOk = 0;
+end
+if actual.model_subspace_id ~= expected.model_subspace_id
+    isOk = 0;
+end
+
+names = fieldnames(expected.estimated_parameters);
+for ipar = 1:length(expected.estimated_parameters)
+    if round(actual.estimated_parameters.(names{ipar}), 3) ~= round(expected.estimated_parameters.(names{ipar}), 3)
+        isOk = 0;
+    end
+end
+end
