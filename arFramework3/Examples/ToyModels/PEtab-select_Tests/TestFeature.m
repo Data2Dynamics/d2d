3fd8d3946728bf6fb3970b9d531287c085924ef9
fgwieland <franz-georg.wieland@fdm.uni-freiburg.de>
2022-01-18 15:39:21 +0100

    Updates to TestFeature for Windows compatability and results output.

--- arFramework3/Examples/ToyModels/PEtab-select_Tests/TestFeature.m
+++ arFramework3/Examples/ToyModels/PEtab-select_Tests/TestFeature.m
@@ -6,20 +6,42 @@ cases = {'0001', '0002', '0003', '0004', '0005', '0006', '0007', '0008'};
 Ncases = numel(cases);
 
 isOk = NaN(Ncases,1);
+
+%% Check the python environment of system command 
+initstr = [];
+syscom = [initstr, 'petab_select --help'];
+[status1,~] = system(syscom);
+if status1 ~= 0
+    initstr = '~/_d2d_python_venv/bin/activate';
+    syscom = [initstr, 'petab_select --help'];
+    [status2,~] = system(syscom);
+    if status2 ~= 0
+        error(sprintf('Calling petab_select from the command line failed. ...\nPlease check your Python environment and the PEtab-select installation.'))
+    end
+end
+
 %parpool(4)
+
 for i = 1:Ncases
     cd(cases{i})
-    system('rm -r output')
-    try
-    arPEtabSelect('~/_d2d_python_venv/bin/activate')    
     
-    expected = ReadYaml('expected.yaml');
-    actual = ReadYaml('petab-select/selected_model.yaml');
-    selProblem = ReadYaml('petab_select_problem.yaml');
-    isOk(i) = comparePeTabYamlStruct(expected, actual, selProblem.criterion);
-    if isOk(i) ~= 1
-        fprintf('test case %s failed\n', cases{i})
+    [status,msg,messageid] = rmdir('output','s');
+    if status==0
+        if  ~strcmp('MATLAB:RMDIR:NotADirectory',messageid)
+            fprintf( 2, 'Error while removing folder output.\n Error message: %s\n', msg);
+        end
     end
+   
+    try
+        arPEtabSelect(initstr)
+        
+        expected = ReadYaml('expected.yaml');
+        actual = ReadYaml('petab-select/selected_model.yaml');
+        selProblem = ReadYaml('petab_select_problem.yaml');
+        isOk(i) = comparePeTabYamlStruct(expected, actual, selProblem.criterion);
+        if isOk(i) ~= 1
+            fprintf('test case %s failed\n', cases{i})
+        end
     catch
         fprintf('error during test of case %s\n', cases{i})
     end
@@ -27,9 +49,14 @@ for i = 1:Ncases
 end
 
 if sum(isOk) == Ncases
+    fprintf( 2, 'Correct results all test cases: %s\n', strjoin(cases(logical(isOk(isOk==1)))));
     fprintf( 2, 'PASSED\n' );
 else
-    fprintf( 2, 'Errors in test case(s) %s\n', strjoin(cases(logical(~Working)),', '));
+    isOk(isnan(isOk)) = 2;
+    
+    fprintf( 2, 'Correct results in test case(s) %s\n', strjoin(cases(logical(isOk(isOk==1)))));
+    fprintf( 2, 'Results not identical in test case(s) %s\n', strjoin(cases(logical(isOk(isOk==0)))));
+    fprintf( 2, 'Errors in test case(s) %s\n', strjoin(cases(logical(isOk(isOk==2)))));
     error( 'FAILED');
 end
 
