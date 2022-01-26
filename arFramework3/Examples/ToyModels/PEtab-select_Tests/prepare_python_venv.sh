c104652efbba7adb662498465ceb0199606a2e4c
Adrian Hauber <33121940+adrianhauber@users.noreply.github.com>
2022-01-26 15:29:54 +0100

    update for internal use on server

--- arFramework3/Examples/ToyModels/PEtab-select_Tests/prepare_python_venv.sh
+++ arFramework3/Examples/ToyModels/PEtab-select_Tests/prepare_python_venv.sh
@@ -1,4 +1,4 @@
-# This script will create a Python virtual environment (venv) as well as the Python packages PEtab & PEtab-select which are necessary to use arPEtabSelect in Data2Dynamics
+# This script will create a Python virtual environment (venv) as well as the Python packages PEtab & PEtab-select which are necessary to use arPEtabSelect i$
 # Execute by navigating to this directory in the terminal and typing "bash prepare_python_venv.sh"
 # Assumes a symlink named "python3.7" that calls Python 3.7
 
@@ -9,6 +9,6 @@ python3.7 -m venv ~/_d2d_python_venv
 source ~/_d2d_python_venv/bin/activate
 
 # install PEtab and PEtab-select
-Pip install --upgrade pip
-Pip install PEtab --upgrade
-Pip install PEtab-select --upgrade
+pip3 install --upgrade pip
+pip3 install PEtab --upgrade
+pip3 install PEtab-select --upgrade
