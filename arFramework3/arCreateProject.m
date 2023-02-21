28a28b1192df61eef2daf8931c96c674b0a7b7d1
ckreutz <ckreutz@imbi.uni-freiburg.de>
2023-02-21 20:29:36 +0100

    arCreateProject is now feasible with different model templates

--- arFramework3/arCreateProject.m
+++ arFramework3/arCreateProject.m
@@ -1,9 +1,50 @@
 % arCreateProject()
 % 
 %    create D2D file structure and template .def files
+% 
+%    model_template   Which model def to choose, either one of the
+%           following abbreviations or directly a model def name 
+%           (see folder arFramework3/ProjectTemplate)
+%           Default:  'model_template_ODEs.def'
+% 
+%           ode       the most basic ODE example 'model_template_ODEs.def'
+%           ode_input ODEs with an input
+%           abc       ODEs für A -> B -> C
+%           hill      Hill functions
 
-function arCreateProject
+function arCreateProject(model_template)
+if ~exist('model_template','var') || isempty(model_template)
+    model_template = 'model_template_ODEs.def';
+end
 
+%% Handle choice of model and data template:
+% replace if short name for model_template was used:
+switch lower(model_template) 
+    case {'ode','odes'} 
+        model_template = 'model_template_ODEs.def';
+    case {'ode_input','odes_input'} 
+        model_template = 'model_template_ODEs_withInput.def';
+    case {'abc'} 
+        model_template = 'model_template_ABC.def';
+    case {'hill','hillfunction'}
+        model_template = 'model_template_HillFunctions.def';
+    otherwise
+        % do nothing
+end
+
+% assignment from model template to data template:
+switch model_template
+    case 'model_template_ABC.def'
+        data_template = 'data_template_ABC.def';
+    case 'model_template_HillFunctions.def'
+        data_template = 'data_template_HillFunctions.def';
+    otherwise % choose the default template
+        data_template = 'data_template.def';
+end
+
+fprintf('New project using %s as model template, %s as data template.\n',model_template,data_template);
+
+%%
 name = input('enter new project name: ', 's');
 name = strrep(name,' ','_');
 
@@ -21,7 +62,7 @@ try
             
             mkdir('Models');
             cd('Models');
-            w = which('model_template.def');
+            w = which(model_template);
             if isempty(w)                
                 error('model_template.def not found. Please check your path and/or execute arInit first.')
             end            
@@ -30,8 +71,8 @@ try
             
             mkdir('Data');
             cd('Data');
-            copyfile(which('data_template.def'),'./data_template.def');
-            copyfile(which('data_template.xls'),'./data_template.xls');
+            copyfile(which(data_template),'./data_template.def');
+            copyfile(which(strrep(data_template,'.def','.xls')),'./data_template.xls');
             cd ..
             
             fid = fopen('./Setup.m' , 'W');
@@ -42,6 +83,7 @@ try
             fprintf(fid, 'arCompileAll;\n');
             
             fclose(fid);
+            edit ./Setup.m
         end
     end
 catch ERR
