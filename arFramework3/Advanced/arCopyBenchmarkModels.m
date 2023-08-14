e8dc2c7fa7127c85fb9438b93a0600ad4d9529eb
niklasneubrand <niklas.neubrand@uniklinik-freiburg.de>
2023-08-14 09:14:35 +0200

    Extension and cleanup of arCopyBenchmarkModels

--- arFramework3/Advanced/arCopyBenchmarkModels.m
+++ arFramework3/Advanced/arCopyBenchmarkModels.m
@@ -13,6 +13,13 @@
 % 
 %               'fast10' 
 %               the 10 models with least computation times
+%
+%               'new'
+%               Three models that have been added to benchmark collection after
+%               publication of Hass et al, 2018
+%
+%               'all' or '23'
+%               The 20 original benchmark models plus the three new models
 % 
 %               The (starting characters of) model names can also be
 %               specified, e.g.:
@@ -70,6 +77,14 @@ models20 = {
             'Zheng_PNAS2012'};
 
 
+% models from benchmarking collection not yet included in Hass et al, 2019
+new_models = {
+              'Borghans_BiophysChem_1997', ...
+              'Elowitz_Nature2000', ...
+              'Sneyd_PNAS2002'};
+all_models = sort([models20, new_models]);
+
+
 switch lower(subset)
     case {'20',''} % default
         models = models20;
@@ -112,52 +127,69 @@ switch lower(subset)
             'Swameye_PNAS2003',...
             'Weber_BMC2015',...
             'Zheng_PNAS2012'};
+    case 'new'
+        models = new_models;
+    case {'all', '23'}
+        models = all_models;
     otherwise
-        ind = strmatch(lower(subset),lower(models20));
-        if ~isempty(ind)
-            models = models20(ind);
-        else
+        % find models that start with the string subset
+        ind = startsWith(all_models, subset, IgnoreCase=true);
+        models = all_models(ind);
+        if isempty(models)
             error('Subset unknown');
         end
 end
 
-DoCopy(models, target_path, subset);
+DoCopy(models, target_path);
 
 
 
 
 
-function DoCopy(models, target_path, subset)
+function DoCopy(models, target_path)
 
 source_path = [fileparts(which('arInit')),filesep,'Examples'];
 
 for i=1:length(models)
+
     fprintf('Copying %s ...\n',models{i});
     copyfile([source_path,filesep,models{i}],[target_path,filesep,models{i}]);
-    
-    switch lower(subset) % model-specific operatios which has to be done:
-        case {'20','','lhsok'}
-            if strcmp(models{i},'Merkle_JAK2STAT5_PCB2016')==1  % just keep SetupFinal.m
-                delete([target_path,filesep,models{i},filesep,'SetupCFUE.m']);
-                delete([target_path,filesep,models{i},filesep,'SetupComprehensive.m']);
-                delete([target_path,filesep,models{i},filesep,'SetupH838.m']);
-                delete([target_path,filesep,models{i},filesep,'SetupSens.m']);
-            end
-            if strcmp(models{i},'Lucarelli_TGFb_2017')==1  % just keep reduced model with genes
-                rmdir([target_path,filesep,models{i},filesep,'TGFb_ComplexModel_ModelSelection_L1'],'s');
-                rmdir([target_path,filesep,models{i},filesep,'TGFb_ComplexModel_withGenes'],'s');
-                movefile([target_path,filesep,models{i},filesep,'TGFb_ComplexModel_WithGenes_Reduced',filesep,'*'],...
-                    [target_path,filesep,models{i},filesep,'.']);
-                rmdir([target_path,filesep,models{i},filesep,'TGFb_ComplexModel_WithGenes_Reduced'],'s');
-            end
-            if strcmp(models{i},'Sobotta_Frontiers2017')==1  % just keep Setup_Core.m
-                delete([target_path,filesep,models{i},filesep,'Setup_IL6_ExpDesign.m']);
-                delete([target_path,filesep,models{i},filesep,'Setup_IL6_Full.m']);
-            end
-            if strcmp(models{i},'Swameye_PNAS2003')==1  % just keep Setup.m
-                delete([target_path,filesep,models{i},filesep,'Setup2003.m']);                
-            end
+
+    % model-specific operations which have to be done:
+    switch models{i}
+        case 'Becker_Science2010'
+            % use only original model, not Setup_Regularization.m
+            delete([target_path,filesep,models{i},filesep,'Setup_Regularization.m']);
+
+        case 'Boehm_JProteomeRes2014'
+            % only keep Setup.m
+            delete([target_path,filesep,models{i},filesep,'Setup_FullModel_Boehm2014.m']);
+        
+        case 'Merkle_JAK2STAT5_PCB2016'
+            % only keep SetupFinal.m
+            delete([target_path,filesep,models{i},filesep,'SetupCFUE.m']);
+            delete([target_path,filesep,models{i},filesep,'SetupComprehensive.m']);
+            delete([target_path,filesep,models{i},filesep,'SetupH838.m']);
+            delete([target_path,filesep,models{i},filesep,'SetupSens.m']);
+        
+        case 'Lucarelli_TGFb_2017'
+            % just keep reduced model with genes
+            rmdir([target_path,filesep,models{i},filesep,'TGFb_ComplexModel_ModelSelection_L1'],'s');
+            rmdir([target_path,filesep,models{i},filesep,'TGFb_ComplexModel_withGenes'],'s');
+            movefile([target_path,filesep,models{i},filesep,'TGFb_ComplexModel_WithGenes_Reduced',filesep,'*'],...
+                     [target_path,filesep,models{i},filesep,'.']);
+            rmdir([target_path,filesep,models{i},filesep,'TGFb_ComplexModel_WithGenes_Reduced'],'s');
+
+        case 'Sobotta_Frontiers2017'
+            % just keep Setup_Core.m
+            delete([target_path,filesep,models{i},filesep,'Setup_IL6_ExpDesign.m']);
+            delete([target_path,filesep,models{i},filesep,'Setup_IL6_Full.m']);
+
+        case 'Swameye_PNAS2003'
+            % just keep Setup.m
+            delete([target_path,filesep,models{i},filesep,'Setup2003.m']);
     end
+
 end
 
 
